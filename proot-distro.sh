b50ebeba7aa1b41caecb450829e3fa12643bdc65
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-09 23:42:51 +0300

    version 0.10

--- proot-distro.sh
+++ proot-distro.sh
@@ -18,7 +18,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="0.9"
+PROGRAM_VERSION="0.10"
 
 #############################################################################
 #
