999482a4b973b83fc56a8aaf367d882a3a247d95
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-10 19:37:57 +0200

    alpine: use https links

--- distro-plugins/alpine.sh
+++ distro-plugins/alpine.sh
@@ -8,16 +8,16 @@ DISTRO_NAME="Alpine Linux 3.12.0"
 get_download_url() {
 	case "$(uname -m)" in
 		aarch64)
-			echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/aarch64/alpine-minirootfs-3.12.0-aarch64.tar.gz"
+			echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/aarch64/alpine-minirootfs-3.12.0-aarch64.tar.gz"
 			;;
 		armv7l|armv8l)
-			echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/armv7/alpine-minirootfs-3.12.0-armv7.tar.gz"
+			echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/armv7/alpine-minirootfs-3.12.0-armv7.tar.gz"
 			;;
 		i686)
-			echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86/alpine-minirootfs-3.12.0-x86.tar.gz"
+			echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86/alpine-minirootfs-3.12.0-x86.tar.gz"
 			;;
 		x86_64)
-			echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.0-x86_64.tar.gz"
+			echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.0-x86_64.tar.gz"
 			;;
 	esac
 }
