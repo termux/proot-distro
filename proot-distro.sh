0725fb3c6540af0183a191b56b28cf4f75d127f7
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-11 15:54:20 +0200

    version 1.3.3

--- proot-distro.sh
+++ proot-distro.sh
@@ -18,7 +18,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="1.3.2"
+PROGRAM_VERSION="1.3.3"
 
 #############################################################################
 #
