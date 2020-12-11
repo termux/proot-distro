77e6de4a59d66bb09aedf60155592e4173963a91
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-11 15:47:05 +0200

    backup: do not hard-reset permissions
    Just ensure that directories are at least RX by user and files are
    at least R.

--- proot-distro.sh
+++ proot-distro.sh
@@ -1337,9 +1337,9 @@ command_backup() {
 
 	msg "${BLUE}[${GREEN}*${BLUE}] ${CYAN}Fixing file permissions in rootfs...${RST}"
 	# Ensure we can read all files.
-	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type d -print0 | xargs -0 -r chmod 700
-	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type f -executable -print0 | xargs -0 -r chmod 700
-	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type f ! -executable -print0 | xargs -0 -r chmod 600
+	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type d -print0 | xargs -0 -r chmod u+rx
+	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type f -executable -print0 | xargs -0 -r chmod u+rx
+	find "${INSTALLED_ROOTFS_DIR}/${distro_name}" -type f ! -executable -print0 | xargs -0 -r chmod u+r
 
 	local distro_plugin_script="${distro_name}.sh"
 	if [ ! -f "${DISTRO_PLUGINS_DIR}/${distro_plugin_script}" ]; then
