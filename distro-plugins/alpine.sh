64101d52035e097dc0ec23978e2006e13fb81568
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-03 18:06:26 +0200

    alpine: update rootfs to Alpine Linux v3.13.1

--- distro-plugins/alpine.sh
+++ distro-plugins/alpine.sh
@@ -2,7 +2,7 @@
 ## Plug-in for installing Alpine Linux.
 ##
 
-DISTRO_NAME="Alpine Linux 3.12.0"
+DISTRO_NAME="Alpine Linux 3.13.1"
 
 # You can override a CPU architecture to let distribution
 # be executed by QEMU (user-mode).
@@ -26,20 +26,20 @@ get_download_url() {
 
 	case "$DISTRO_ARCH" in
 		aarch64)
-			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.12.0-aarch64.tar.gz"
-			sha256="3f5c39b088ddaf44910eed4427f7d511b5e4aa7041943221baf2d4734c5fa60a"
+			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-aarch64.tar.gz"
+			sha256="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
 			;;
 		armv7l|armv8l)
-			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.12.0-armv7.tar.gz"
-			sha256="d4842c7c65fed4291ca3eea7e50a918aec368b66663906220c4887eee14ae176"
+			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-armv7.tar.gz"
+			sha256="71bedb0810b656377192f4a239126f452a016d7a33e28f8ad8f46e9b0e7d8028"
 			;;
 		i686)
-			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.12.0-x86.tar.gz"
-			sha256="f06ae2ed0b5f52457a9762ddfcd067f559d35f92b83b4d0a294e3001e5070a62"
+			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-x86.tar.gz"
+			sha256="6884df8f8e44d462a9723e412f0b76124019683d98afe448a8767dc5f5f3a7e0"
 			;;
 		x86_64)
-			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.12.0-x86_64.tar.gz"
-			sha256="0beb54cf9bf69d085f9fcd291ff28b3335184d08b706d535f425e8180851edc9"
+			rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-x86_64.tar.gz"
+			sha256="1ab0a7c05107c4504018be170d937c07e35283b7a3f153886c8c93162dfbf4af"
 			;;
 	esac
 
