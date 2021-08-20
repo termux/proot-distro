319272bde79fd4757c9bc3f892c138277ac5e276
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2021-08-20 08:38:18 +0800

    Update ubuntu.sh
    Added  'DISTRO_ARCH'

--- distro-plugins/ubuntu.sh
+++ distro-plugins/ubuntu.sh
@@ -1,7 +1,9 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
+# You can overide an architecture by inserting 'DISTRO_ARCH=' , but it will be overwritten on next update. 
 DISTRO_NAME="Ubuntu (20.04)"
+
 
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.2.0/ubuntu-aarch64-pd-v2.2.0.tar.xz"
 TARBALL_SHA256['aarch64']="486de37668963c1b6a0d131e33b91486be8eb1919b0813ad03726885753feba6"
