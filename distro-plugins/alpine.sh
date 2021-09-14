138fa1d9abfa1729e245797831bc5005175328dc
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-14 13:23:09 +0300

    alpine: switch to edge distribution
    Rolling release + more packages available in repos.

--- distro-plugins/alpine.sh
+++ distro-plugins/alpine.sh
@@ -1,13 +1,13 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
-DISTRO_NAME="Alpine Linux (3.14.1)"
+DISTRO_NAME="Alpine Linux (edge)"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/alpine-aarch64-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['aarch64']="9e010ed08fcae1748d558001eb949f7d9201c7e78a41bc76aee2aaebacac11e1"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v1.10.1/alpine-arm-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['arm']="97916333cd7b46903b82add88c0166427ceae7209d363e86fbbd0a789300487f"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v1.10.1/alpine-i686-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['i686']="f17df7e3e4933457e94f1b0c7977187868e5c90e561f5e7e0fe07453e6f66aca"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/alpine-x86_64-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['x86_64']="eaf74a8b0d37886eaf88d51b51bf0b67b006086a02e5d7bb8bb6cdf148110c5a"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.5.0/alpine-aarch64-pd-v2.5.0.tar.xz"
+TARBALL_SHA256['aarch64']="9c1e8571ce06e739967a7e3bd7ac9cfc4b8421905ef18f5d7b0e56910fe402b3"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v2.5.0/alpine-arm-pd-v2.5.0.tar.xz"
+TARBALL_SHA256['arm']="58d1dbed91ba20599fd12a63ef9b726b2b4bf6bea7a7d2957842768aff0b7f5e"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v2.5.0/alpine-i686-pd-v2.5.0.tar.xz"
+TARBALL_SHA256['i686']="dee9d7f3a03318a71cfd11516bb2bb5fcac7382ee0270e7d01528146c0de0f94"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v2.5.0/alpine-x86_64-pd-v2.5.0.tar.xz"
+TARBALL_SHA256['x86_64']="80097be9cfedcf8f1d9128968356e8c14e7ab15abed51ca6936be69025db694c"
