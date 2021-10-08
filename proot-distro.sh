2efd6319f5e9450cbe5e4e633bf3d08b9611f438
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-08 17:29:31 +0300

    version 2.6.4

--- proot-distro.sh
+++ proot-distro.sh
@@ -18,7 +18,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="2.6.3"
+PROGRAM_VERSION="2.6.4"
 
 #############################################################################
 #
