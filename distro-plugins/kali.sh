bcca85e35fbd6ca1cdc6f57830849889dbf90c05
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2021-10-25 01:17:06 +0600

    Update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -5,7 +5,7 @@ TARBALL_URL['arm']="https://github.com/BDhackers009/nethunter-rootfs-termux/rele
 TARBALL_SHA256['arm']="365c4483fbef46624381c2f34a4e49080f8930564ba984971b56b373012fe866"
 
 distro_setup() {
-	run_proot_cmd sed -i 's/http:\/\//[trusted=yes]http:\/\//g' /etc/apt/sources.list
-	run_proot_cmd apt update && apt install gnupg -y
-	run_proot_cmd apt-key adv --keyserver  http://keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
+        run_proot_cmd echo "deb [trusted=yes]  http://http.kali.org/kali kali-rolling main contrib non-free" > /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/etc/apt/sources.list
+        run_proot_cmd echo "#deb-src [trusted=yes]  http://http.kali.org/kali kali-rolling main non-free contrib"  >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/etc/apt/sources.list
+        run_proot_cmd echo "apt update && apt install gnupg dialog apt-utils -y && apt-key adv --keyserver  http://keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6"  > /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/root/fix.sh	
 }
