1e7766d951229aac0ba29341d87d0ca1e3600437
Egor Gavrilov <92942581+gavrilovegor519@users.noreply.github.com>
2021-10-25 10:05:29 +0300

    Update gentoo.sh

--- distro-plugins/gentoo.sh
+++ distro-plugins/gentoo.sh
@@ -15,7 +15,7 @@ TARBALL_SHA256['x86_64']="9bdefd5dd3eed63ace9416de9c75b3c9d0ca5d3f37abc0fb23af1c
 distro_setup() {
         if [ "$DISTRO_ARCH" = "aarch64" ]; then
 	  run_proot_cmd curl -LO http://distfiles.gentoo.org/experimental/prefix/arm/prefix-stage3-arm64-latest.tar.xz
-	  run_proot_cmd tar -xpf prefix-stage3-arm64-latest.tar.xz
+	  run_proot_cmd tar -xpf prefix-stage3-arm64-latest.tar.xz --xattrs-include='*.*' --numeric-owner
             run_proot_cmd rm -f /usr/bin/patch
             run_proot_cmd mv ~/gentoo64/usr/bin/patch /usr/bin/patch
             run_proot_cmd echo "USE+=\" -xattr\"" >> /etc/portage/make.conf
@@ -23,7 +23,7 @@ distro_setup() {
             run_proot_cmd rm -rf ~/gentoo64 ~/prefix-stage3-arm64-latest.tar.xz
         elif [ "$DISTRO_ARCH" = "arm" ]; then
 	  run_proot_cmd curl -LO http://distfiles.gentoo.org/experimental/prefix/arm/prefix-stage3-armv7a_hardfp-latest.tar.xz
-	  run_proot_cmd tar -C -xpf prefix-stage3-armv7a_hardfp-latest.tar.xz
+	  run_proot_cmd tar -C -xpf prefix-stage3-armv7a_hardfp-latest.tar.xz --xattrs-include='*.*' --numeric-owner
             run_proot_cmd rm -f /usr/bin/patch
             run_proot_cmd mv ~/gentoo/usr/bin/patch /usr/bin/patch
             run_proot_cmd echo "USE+=\" -xattr\"" >> /etc/portage/make.conf
