47689c14aa43c2f5d026de9c6d81ac2d9cd72121
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-25 15:55:12 +0300

    version 2.7.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -18,7 +18,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="2.6.9"
+PROGRAM_VERSION="2.7.0"
 
 #############################################################################
 #
