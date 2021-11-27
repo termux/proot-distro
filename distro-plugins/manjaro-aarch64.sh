41c2b86005bdaff49e643552724e567b25ce8e94
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-11-27 18:23:57 +0200

    new distribution: manjaro (aarch64)

---
+++ distro-plugins/manjaro-aarch64.sh
@@ -0,0 +1,8 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Manjaro AArch64"
+DISTRO_COMMENT="Only for AArch64 hosts."
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.8.0/manjaro-aarch64-pd-v2.8.0.tar.xz"
+TARBALL_SHA256['aarch64']="fc7eff8a5fe2e3ef695af833d8dc59624d2a727d68a393ace5280c0e3c433c6f"
