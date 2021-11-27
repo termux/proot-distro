dedb22a6707027bc8775fbf191af6427a24adf0e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-11-27 18:07:26 +0200

    bootstrap-rootfs.sh: add build steps for manjaro (aarch64 only)

--- bootstrap-rootfs.sh
+++ bootstrap-rootfs.sh
@@ -292,6 +292,32 @@ TARBALL_URL['x86_64']="${GIT_RELEASE_URL}/fedora-x86_64-pd-${CURRENT_VERSION}.ta
 TARBALL_SHA256['x86_64']="$(sha256sum "${ROOTFS_DIR}/fedora-x86_64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
 EOF
 
+# Manjaro AArch64.
+# Repack only.
+printf "\n[*] Building Manjaro AArch64...\n"
+curl --fail --location \
+	--output "${WORKDIR}/manjaro-aarch64.tar.xz" \
+	"https://osdn.net/projects/manjaro-arm/storage/.rootfs/Manjaro-ARM-aarch64-latest.tar.gz"
+
+mkdir "${WORKDIR}/manjaro-aarch64"
+sudo tar -xpf \
+	-f "${WORKDIR}/manjaro-aarch64.tar.xz" \
+	-C "${WORKDIR}/manjaro-aarch64"
+
+sudo tar -Jcf "${ROOTFS_DIR}/manjaro-aarch64-pd-${CURRENT_VERSION}.tar.xz" \
+	-C "${WORKDIR}/manjaro-aarch64" ./
+sudo chown $(id -un):$(id -gn) "${ROOTFS_DIR}/manjaro-aarch64-pd-${CURRENT_VERSION}.tar.xz"
+
+cat <<- EOF > "${PLUGIN_DIR}/manjaro-aarch64.sh"
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Manjaro AArch64"
+
+TARBALL_URL['aarch64']="${GIT_RELEASE_URL}/manjaro-aarch64-pd-${CURRENT_VERSION}.tar.xz"
+TARBALL_SHA256['aarch64']="$(sha256sum "${ROOTFS_DIR}/manjaro-aarch64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
+EOF
+
 # OpenSUSE.
 # Extracting from Docker image.
 printf "\n[*] Building OpenSUSE...\n"
