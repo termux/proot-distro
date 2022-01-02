0976cb1e66b58771a10f5ec47f8208e55044d8c7
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2022-01-02 12:35:31 +0600

    Update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="Kali Linux (nethunter)"
 TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/fs-cook/releases/download/kali-01/kali-rolling-raw-arm64.tar.gz"
-TARBALL_SHA256['aarch64']="ec849f556768b9d9bdb541a32760ad2c00d73bf3aff8436e203cadb33d14bea3"
+TARBALL_SHA256['aarch64']="bcd311ae53fc6576881e5f8cbbc007fc7c49a5348894e03fa97cc2397ef24fff"
 TARBALL_URL['arm']="https://github.com/BDhackers009/nethunter-rootfs-termux/releases/download/69.69/kalifs-armhf-minimal.tar.xz"
 TARBALL_SHA256['arm']="365c4483fbef46624381c2f34a4e49080f8930564ba984971b56b373012fe866"
 
