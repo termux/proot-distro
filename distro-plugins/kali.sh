56ee172d787179c285bed1297d1bbc9e37bbdd0c
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2022-01-02 18:31:42 +0600

    update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,8 +1,8 @@
 DISTRO_NAME="Kali Linux (nethunter)"
-TARBALL_URL['aarch64']="https://github.com/BDhackers009/nethunter-rootfs-termux/releases/download/69.69/kalifs-arm64-minimal.tar.xz"
-TARBALL_SHA256['aarch64']="bcd311ae53fc6576881e5f8cbbc007fc7c49a5348894e03fa97cc2397ef24fff"
-TARBALL_URL['arm']="https://github.com/BDhackers009/nethunter-rootfs-termux/releases/download/69.69/kalifs-armhf-minimal.tar.xz"
-TARBALL_SHA256['arm']="365c4483fbef46624381c2f34a4e49080f8930564ba984971b56b373012fe866"
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-nano.tar.xz"
+TARBALL_SHA256['aarch64']="520ba187d3f20883f76d032b5a0ae6d369de55e7115d67d3ad57298b4d7687ec"
+TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
+TARBALL_SHA256['arm']="d7b0b8b91b84a502c44a320c691551a727472657d060af3973517fbadddda7b8"
 
 distro_setup() {
         run_proot_cmd echo "deb [trusted=yes]  http://http.kali.org/kali kali-rolling main contrib non-free" > /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/etc/apt/sources.list
