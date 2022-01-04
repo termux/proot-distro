ccdba7145c02492a7029068ef61ac0b0bad83673
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2022-01-04 16:15:49 +0600

    update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -4,8 +4,3 @@ TARBALL_SHA256['aarch64']="520ba187d3f20883f76d032b5a0ae6d369de55e7115d67d3ad572
 TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
 TARBALL_SHA256['arm']="d7b0b8b91b84a502c44a320c691551a727472657d060af3973517fbadddda7b8"
 
-distro_setup() {
-  
-  run_proot_cmd 'curl https://raw.githubusercontent.com/BDhackers009/trash/main/user.sh --output /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/root/'
-  
-}
