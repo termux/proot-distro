b0decf671311cb22a19b488184721f199ad92f36
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-01-15 14:07:56 +0200

    login: add fallbacks for bind-mounting storage

--- proot-distro.sh
+++ proot-distro.sh
@@ -1245,8 +1245,29 @@ command_login() {
 			set -- "--bind=/data/dalvik-cache" "$@"
 			set -- "--bind=/data/data/@TERMUX_APP_PACKAGE@/cache" "$@"
 			set -- "--bind=@TERMUX_HOME@" "$@"
-			set -- "--bind=/storage" "$@"
-			set -- "--bind=/storage/self/primary:/sdcard" "$@"
+
+			# Setup bind mounting for shared storage.
+			# We want to use the primary shared storage mount point there
+			# with avoiding secondary and legacy mount points. As Android
+			# OS versions are different, some directories may be unavailable
+			# and we need to try them all.
+			if ls -1U /storage/self/primary/ > /dev/null 2>&1; then
+				set -- "--bind=/storage/self/primary:/sdcard" "$@"
+			elif ls -1U /storage/emulated/0/ > /dev/null 2>&1; then
+				set -- "--bind=/storage/emulated/0:/sdcard" "$@"
+			elif ls -1U /sdcard/ > /dev/null 2>&1; then
+				set -- "--bind=/sdcard:/sdcard" "$@"
+			else
+				# No access to shared storage.
+				:
+			fi
+
+			# /storage also optional bind mounting.
+			# If we can't access it, don't provide this directory
+			# in proot environment.
+			if ls -1U /storage > /dev/null 2>&1; then
+				set -- "--bind=/storage" "$@"
+			fi
 		fi
 
 		# When using QEMU, we need some host files even in isolated mode.
