bf505048e8fb0339c7a89017a1e0baedabd454c7
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2022-02-18 23:20:55 +0600

    update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="Kali Linux (nethunter)"
-TARBALL_URL['aarch64']="https://github.com/BDhackers009/nethunter-termux/releases/download/2.9.4/kalifs-arm64-nano.tar.xz"
-TARBALL_SHA256['aarch64']="520ba187d3f20883f76d032b5a0ae6d369de55e7115d67d3ad57298b4d7687ec"
-TARBALL_URL['arm']="https://github.com/BDhackers009/nethunter-termux/releases/download/2.9.4/kalifs-armhf-nano.tar.xz"
-TARBALL_SHA256['arm']="d7b0b8b91b84a502c44a320c691551a727472657d060af3973517fbadddda7b8"
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-nano.tar.xz"
+TARBALL_SHA256['aarch64']="2aa9e8b1d7561768abd1e2ce8104f1ccc60cbf3072b58e976edf219114808546"
+TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
+TARBALL_SHA256['arm']="9ea566808762960b4f24982f7c0b41da49b6b86417d83fa6573f74feb97e0f0a"
 
