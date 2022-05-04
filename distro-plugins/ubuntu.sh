45871872eafd5424db323df47f96ac5aaa5bfaad
wmcbtech30 <bautista.wyatt.marcus_30@outlook.com>
2022-05-04 10:04:12 +0800

    version 3.0.1

--- distro-plugins/ubuntu.sh
+++ distro-plugins/ubuntu.sh
@@ -1,14 +1,14 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
-DISTRO_NAME="Ubuntu (impish)"
+DISTRO_NAME="Ubuntu (jammy)"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v2.8.0/ubuntu-aarch64-pd-v2.8.0.tar.xz"
-TARBALL_SHA256['aarch64']="e0bb4a06215d39bb8ff3a4469bb69dd78babc39dad9c82cd01e4ac52eba8c7b6"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v2.8.0/ubuntu-arm-pd-v2.8.0.tar.xz"
-TARBALL_SHA256['arm']="e985999097c3dd8692342867c6184b1a922dfee78a575ef25747da98b964a8c9"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v2.8.0/ubuntu-x86_64-pd-v2.8.0.tar.xz"
-TARBALL_SHA256['x86_64']="0c8c19908df6eeae2d963fb00e1153c982b81386a480c16bfe683dfcdea404ec"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.0.1/ubuntu-aarch64-pd-v3.0.1.tar.xz"
+TARBALL_SHA256['aarch64']="a5403d61f40a72a5c1fd0d9ef5368cf08a23b0f40442e1e60fe4601de5c0c2ab"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.0.1/ubuntu-arm-pd-v3.0.1.tar.xz"
+TARBALL_SHA256['arm']="04b736a3cf2ff57027d396013aab778cfe2a6800108a779af480a264f634d8f3"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.0.1/ubuntu-x86_64-pd-v3.0.1.tar.xz"
+TARBALL_SHA256['x86_64']="9ad3e8f4de4a85d8dae3a88c1140da0058d5bbae65a0717bb06f1ccecad0d7a6"
 
 distro_setup() {
 	# Don't update gvfs-daemons and udisks2
