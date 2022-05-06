559847e5107535608df4b1bc6364ece0e80f2540
marcusz <bautista.wyatt.marcus_30@outlook.com>
2022-05-06 15:47:13 +0800

    version 3.0.2

--- proot-distro.sh
+++ proot-distro.sh
@@ -16,7 +16,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="3.0.1"
+PROGRAM_VERSION="3.0.2"
 
 #############################################################################
 #
