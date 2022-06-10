0196437ffc22b7a18d32c41f9cc67fb567ada980
marcusz <bautista.wyatt.marcus_30@outlook.com>
2022-06-10 16:49:43 +0800

    debian: test

---
+++ distro-build/debian-test.sh
@@ -0,0 +1,41 @@
+dist_name="Debian"
+dist_version="bullseye"
+
+bootstrap_distribution() {
+	for arch in arm64 armhf i386 amd64; do
+		sudo mmdebstrap \
+			--architectures=${arch} \
+			--variant=minbase \
+			--components="main,contrib" \
+			--include="dbus-user-session,ca-certificates,gvfs-daemons,libsystemd0,systemd-sysv,udisks2" \
+			--format=tar \
+			"${dist_name}" \
+			"${ROOTFS_DIR}/debian-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar"
+		sudo chown $(id -un):$(id -gn) "${ROOTFS_DIR}/debian-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar"
+		xz "${ROOTFS_DIR}/debian-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar"
+	done
+	unset arch
+}
+
+write_plugin() {
+	cat <<- EOF > "${PLUGIN_DIR}/debian.sh"
+	# This is a default distribution plug-in.
+	# Do not modify this file as your changes will be overwritten on next update.
+	# If you want customize installation, please make a copy.
+	DISTRO_NAME="Debian (${dist_version})"
+
+	TARBALL_URL['aarch64']="${GIT_RELEASE_URL}/debian-aarch64-pd-${CURRENT_VERSION}.tar.xz"
+	TARBALL_SHA256['aarch64']="$(sha256sum "${ROOTFS_DIR}/debian-aarch64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
+	TARBALL_URL['arm']="${GIT_RELEASE_URL}/debian-arm-pd-${CURRENT_VERSION}.tar.xz"
+	TARBALL_SHA256['arm']="$(sha256sum "${ROOTFS_DIR}/debian-arm-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
+	TARBALL_URL['i686']="${GIT_RELEASE_URL}/debian-i686-pd-${CURRENT_VERSION}.tar.xz"
+	TARBALL_SHA256['i686']="$(sha256sum "${ROOTFS_DIR}/debian-i686-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
+	TARBALL_URL['x86_64']="${GIT_RELEASE_URL}/debian-x86_64-pd-${CURRENT_VERSION}.tar.xz"
+	TARBALL_SHA256['x86_64']="$(sha256sum "${ROOTFS_DIR}/debian-x86_64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
+
+	distro_setup() {
+	${TAB}# Don't update gvfs-daemons and udisks2
+	${TAB}run_proot_cmd apt-mark hold gvfs-daemons udisks2
+	}
+	EOF
+}
