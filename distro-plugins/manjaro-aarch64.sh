044ef269ffa113e1187197019d3e708dc16bf856
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-12-25 14:40:02 +0200

    update manjaro

--- distro-plugins/manjaro-aarch64.sh
+++ distro-plugins/manjaro-aarch64.sh
@@ -5,4 +5,4 @@ DISTRO_NAME="Manjaro AArch64"
 DISTRO_COMMENT="Only for AArch64 hosts."
 
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.3.0/manjaro-aarch64-pd-v3.3.0.tar.xz"
-TARBALL_SHA256['aarch64']="065c9a7a1c2a28b0e8addb161f4fb5388d1364a7558ace72a18324933523718d"
+TARBALL_SHA256['aarch64']="bb0dcf5ce124c1b72057355a9b64eb65ab3ad46de7ade1574d932b53e074099f"
