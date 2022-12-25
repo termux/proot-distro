0a017eb0ffbbb5400b77ad409b0657660770af86
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-12-25 16:28:56 +0200

    update archlinux

--- distro-plugins/archlinux.sh
+++ distro-plugins/archlinux.sh
@@ -1,13 +1,9 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
-DISTRO_NAME="Arch Linux"
+DISTRO_NAME="Arch Linux ARM"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/archlinux-aarch64-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['aarch64']="0faef6f391a367424eec971ca256a2f980576283f6b67235eb04e47fefc90e0c"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v1.10.1/archlinux-arm-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['arm']="5b35f8158d6c37b9e6cb13e1753afd5c2b480d6297b73a39b24c2065a2931f67"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v1.10.1/archlinux-i686-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['i686']="272756b993b903fe894aa9d9588e221673b7dbbb3e63ff867b95203b49a5a5a6"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/archlinux-x86_64-pd-v1.10.1.tar.xz"
-TARBALL_SHA256['x86_64']="7bc126d4e13af10be7b9ff2ae3041dee532f8c7c978f82e8163df4937d73a0f4"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.3.0/archlinux-aarch64-pd-v3.3.0.tar.xz"
+TARBALL_SHA256['aarch64']="69841d372219ec5fc816fdc783b5123ffb614e3249be97bd587e98081d1e37dc"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.3.0/archlinux-arm-pd-v3.3.0.tar.xz"
+TARBALL_SHA256['arm']="2b52a277b177f0099e010d8ae683a02397cee9dfee4348451e631e28544a7a8d"
