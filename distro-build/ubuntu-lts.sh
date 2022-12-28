5f06ba93d0f900769e1d42708d4aa7289ce641d5
babaric-dev <104625126+babaric-dev@users.noreply.github.com>
2022-12-28 18:08:07 +0800

    Fix plugin path for LTS

--- distro-build/ubuntu-lts.sh
+++ distro-build/ubuntu-lts.sh
@@ -17,7 +17,7 @@ bootstrap_distribution() {
 }
 
 write_plugin() {
-	cat <<- EOF > "${PLUGIN_DIR}/ubuntu.sh"
+	cat <<- EOF > "${PLUGIN_DIR}/ubuntu-lts.sh"
 	# This is a default distribution plug-in.
 	# Do not modify this file as your changes will be overwritten on next update.
 	# If you want customize installation, please make a copy.
