8094cc5964854d1f14bc1074d2ad850d04076ca5
Leonid Pliushch <sylirre@termux.dev>
2023-03-29 01:21:49 +0300

    install: update fake entries for /proc

--- proot-distro.sh
+++ proot-distro.sh
@@ -866,141 +866,168 @@ run_proot_cmd() {
 			"$@"
 }
 
-# A function for preparing fake content for certain /proc
-# entries which are known to be restricted on Android.
+# A function for preparing fake content for certain /proc entries which are
+# known to have restricted access on Android OS. All entries are based on
+# values retrieved from Arch Linux (x86_64) running on a VM with 8 CPUs and 8
+# GiB of memory. Date 2023.03.28, Linux 6.2.1. Some values edited to fit
+# the PRoot-Distro.
 setup_fake_proc() {
 	mkdir -p "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc"
 	chmod 700 "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc"
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.loadavg" ]; then
 		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.loadavg"
-		0.54 0.41 0.30 1/931 370386
+		0.12 0.07 0.02 2/165 765
 		EOF
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.stat" ]; then
 		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.stat"
-		cpu  1050008 127632 898432 43828767 37203 63 99244 0 0 0
-		cpu0 212383 20476 204704 8389202 7253 42 12597 0 0 0
-		cpu1 224452 24947 215570 8372502 8135 4 42768 0 0 0
-		cpu2 222993 17440 200925 8424262 8069 9 17732 0 0 0
-		cpu3 186835 8775 195974 8486330 5746 3 8360 0 0 0
-		cpu4 107075 32886 48854 8688521 3995 4 5758 0 0 0
-		cpu5 90733 20914 27798 1429573 2984 1 11419 0 0 0
-		intr 53261351 0 686 1 0 0 1 12 31 1 20 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 7818 0 0 0 0 0 0 0 0 255 33 1912 33 0 0 0 0 0 0 3449534 2315885 2150546 2399277 696281 339300 22642 19371 0 0 0 0 0 0 0 0 0 0 0 2199 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 2445 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 162240 14293 2858 0 151709 151592 0 0 0 284534 0 0 0 0 0 0 0 0 0 0 0 0 0 0 185353 0 0 938962 0 0 0 0 736100 0 0 1 1209 27960 0 0 0 0 0 0 0 0 303 115968 452839 2 0 0 0 0 0 0 0 0 0 0 0 0 0 160361 8835 86413 1292 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3592 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 6091 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 35667 0 0 156823 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 138 2667417 0 41 4008 952 16633 533480 0 0 0 0 0 0 262506 0 0 0 0 0 0 126 0 0 1558488 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 2 2 8 0 0 6 0 0 0 10 3 4 0 0 0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 20 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 12 1 1 83806 0 1 1 0 1 0 1 1 319686 2 8 0 0 0 0 0 0 0 0 0 244534 0 1 10 9 0 10 112 107 40 221 0 0 0 144
-		ctxt 90182396
-		btime 1595203295
-		processes 270853
+		cpu  1957 0 2877 93280 262 342 254 87 0 0
+		cpu0 31 0 226 12027 82 10 4 9 0 0
+		cpu1 45 0 664 11144 21 263 233 12 0 0
+		cpu2 494 0 537 11283 27 10 3 8 0 0
+		cpu3 359 0 234 11723 24 26 5 7 0 0
+		cpu4 295 0 268 11772 10 12 2 12 0 0
+		cpu5 270 0 251 11833 15 3 1 10 0 0
+		cpu6 430 0 520 11386 30 8 1 12 0 0
+		cpu7 30 0 172 12108 50 8 1 13 0 0
+		intr 127541 38 290 0 0 0 0 4 0 1 0 0 25329 258 0 5777 277 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+		ctxt 140223
+		btime 1680020856
+		processes 772
 		procs_running 2
 		procs_blocked 0
-		softirq 25293348 2883 7658936 40779 539155 497187 2864 1908702 7229194 279723 7133925
+		softirq 75663 0 5903 6 25375 10774 0 243 11685 0 21677
 		EOF
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.uptime" ]; then
 		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.uptime"
-		284684.56 513853.46
+		124.08 932.80
 		EOF
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.version" ]; then
 		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.version"
-		Linux version 5.4.0-faked (termux@androidos) (gcc version 4.9.x (Faked /proc/version by Proot-Distro) ) #1 SMP PREEMPT Fri Jul 10 00:00:00 UTC 2020
+		Linux version ${DEFAULT_FAKE_KERNEL_VERSION} (proot@termux) (gcc (GCC) 12.2.1 20230201, GNU ld (GNU Binutils) 2.40) #1 SMP PREEMPT_DYNAMIC Wed, 01 Mar 2023 00:00:00 +0000
 		EOF
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.vmstat" ]; then
 		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.vmstat"
-		nr_free_pages 146031
-		nr_zone_inactive_anon 196744
-		nr_zone_active_anon 301503
-		nr_zone_inactive_file 2457066
-		nr_zone_active_file 729742
-		nr_zone_unevictable 164
-		nr_zone_write_pending 8
-		nr_mlock 34
-		nr_page_table_pages 6925
-		nr_kernel_stack 13216
+		nr_free_pages 1743136
+		nr_zone_inactive_anon 179281
+		nr_zone_active_anon 7183
+		nr_zone_inactive_file 22858
+		nr_zone_active_file 51328
+		nr_zone_unevictable 642
+		nr_zone_write_pending 0
+		nr_mlock 0
 		nr_bounce 0
 		nr_zspages 0
 		nr_free_cma 0
-		numa_hit 672391199
+		numa_hit 1259626
 		numa_miss 0
 		numa_foreign 0
-		numa_interleave 62816
-		numa_local 672391199
+		numa_interleave 720
+		numa_local 1259626
 		numa_other 0
-		nr_inactive_anon 196744
-		nr_active_anon 301503
-		nr_inactive_file 2457066
-		nr_active_file 729742
-		nr_unevictable 164
-		nr_slab_reclaimable 132891
-		nr_slab_unreclaimable 38582
+		nr_inactive_anon 179281
+		nr_active_anon 7183
+		nr_inactive_file 22858
+		nr_active_file 51328
+		nr_unevictable 642
+		nr_slab_reclaimable 8091
+		nr_slab_unreclaimable 7804
 		nr_isolated_anon 0
 		nr_isolated_file 0
-		workingset_nodes 25623
-		workingset_refault 46689297
-		workingset_activate 4043141
-		workingset_restore 413848
-		workingset_nodereclaim 35082
-		nr_anon_pages 599893
-		nr_mapped 136339
-		nr_file_pages 3086333
-		nr_dirty 8
+		workingset_nodes 0
+		workingset_refault_anon 0
+		workingset_refault_file 0
+		workingset_activate_anon 0
+		workingset_activate_file 0
+		workingset_restore_anon 0
+		workingset_restore_file 0
+		workingset_nodereclaim 0
+		nr_anon_pages 7723
+		nr_mapped 8905
+		nr_file_pages 253569
+		nr_dirty 0
 		nr_writeback 0
 		nr_writeback_temp 0
-		nr_shmem 13743
+		nr_shmem 178741
 		nr_shmem_hugepages 0
 		nr_shmem_pmdmapped 0
 		nr_file_hugepages 0
 		nr_file_pmdmapped 0
-		nr_anon_transparent_hugepages 57
-		nr_unstable 0
-		nr_vmscan_write 57250
-		nr_vmscan_immediate_reclaim 2673
-		nr_dirtied 79585373
-		nr_written 72662315
+		nr_anon_transparent_hugepages 1
+		nr_vmscan_write 0
+		nr_vmscan_immediate_reclaim 0
+		nr_dirtied 0
+		nr_written 0
+		nr_throttled_written 0
 		nr_kernel_misc_reclaimable 0
-		nr_dirty_threshold 657954
-		nr_dirty_background_threshold 328575
-		pgpgin 372097889
-		pgpgout 296950969
-		pswpin 14675
-		pswpout 59294
-		pgalloc_dma 4
-		pgalloc_dma32 101793210
-		pgalloc_normal 614157703
+		nr_foll_pin_acquired 0
+		nr_foll_pin_released 0
+		nr_kernel_stack 2780
+		nr_page_table_pages 344
+		nr_sec_page_table_pages 0
+		nr_swapcached 0
+		pgpromote_success 0
+		pgpromote_candidate 0
+		nr_dirty_threshold 356564
+		nr_dirty_background_threshold 178064
+		pgpgin 890508
+		pgpgout 0
+		pswpin 0
+		pswpout 0
+		pgalloc_dma 272
+		pgalloc_dma32 261
+		pgalloc_normal 1328079
 		pgalloc_movable 0
+		pgalloc_device 0
 		allocstall_dma 0
 		allocstall_dma32 0
-		allocstall_normal 184
-		allocstall_movable 239
+		allocstall_normal 0
+		allocstall_movable 0
+		allocstall_device 0
 		pgskip_dma 0
 		pgskip_dma32 0
 		pgskip_normal 0
 		pgskip_movable 0
-		pgfree 716918803
-		pgactivate 68768195
-		pgdeactivate 7278211
-		pglazyfree 1398441
-		pgfault 491284262
-		pgmajfault 86567
-		pglazyfreed 1000581
-		pgrefill 7551461
-		pgsteal_kswapd 130545619
-		pgsteal_direct 205772
-		pgscan_kswapd 131219641
-		pgscan_direct 207173
+		pgskip_device 0
+		pgfree 3077011
+		pgactivate 0
+		pgdeactivate 0
+		pglazyfree 0
+		pgfault 176973
+		pgmajfault 488
+		pglazyfreed 0
+		pgrefill 0
+		pgreuse 19230
+		pgsteal_kswapd 0
+		pgsteal_direct 0
+		pgsteal_khugepaged 0
+		pgdemote_kswapd 0
+		pgdemote_direct 0
+		pgdemote_khugepaged 0
+		pgscan_kswapd 0
+		pgscan_direct 0
+		pgscan_khugepaged 0
 		pgscan_direct_throttle 0
+		pgscan_anon 0
+		pgscan_file 0
+		pgsteal_anon 0
+		pgsteal_file 0
 		zone_reclaim_failed 0
-		pginodesteal 8055
-		slabs_scanned 9977903
-		kswapd_inodesteal 13337022
-		kswapd_low_wmark_hit_quickly 33796
-		kswapd_high_wmark_hit_quickly 3948
-		pageoutrun 43580
-		pgrotated 200299
+		pginodesteal 0
+		slabs_scanned 0
+		kswapd_inodesteal 0
+		kswapd_low_wmark_hit_quickly 0
+		kswapd_high_wmark_hit_quickly 0
+		pageoutrun 0
+		pgrotated 0
 		drop_pagecache 0
 		drop_slab 0
 		oom_kill 0
@@ -1009,46 +1036,64 @@ setup_fake_proc() {
 		numa_hint_faults 0
 		numa_hint_faults_local 0
 		numa_pages_migrated 0
-		pgmigrate_success 768502
-		pgmigrate_fail 1670
-		compact_migrate_scanned 1288646
-		compact_free_scanned 44388226
-		compact_isolated 1575815
-		compact_stall 863
-		compact_fail 392
-		compact_success 471
-		compact_daemon_wake 975
-		compact_daemon_migrate_scanned 613634
-		compact_daemon_free_scanned 26884944
+		pgmigrate_success 0
+		pgmigrate_fail 0
+		thp_migration_success 0
+		thp_migration_fail 0
+		thp_migration_split 0
+		compact_migrate_scanned 0
+		compact_free_scanned 0
+		compact_isolated 0
+		compact_stall 0
+		compact_fail 0
+		compact_success 0
+		compact_daemon_wake 0
+		compact_daemon_migrate_scanned 0
+		compact_daemon_free_scanned 0
 		htlb_buddy_alloc_success 0
 		htlb_buddy_alloc_fail 0
-		unevictable_pgs_culled 258910
-		unevictable_pgs_scanned 3690
-		unevictable_pgs_rescued 200643
-		unevictable_pgs_mlocked 199204
-		unevictable_pgs_munlocked 199164
-		unevictable_pgs_cleared 6
-		unevictable_pgs_stranded 6
-		thp_fault_alloc 10655
-		thp_fault_fallback 130
-		thp_collapse_alloc 655
-		thp_collapse_alloc_failed 50
+		cma_alloc_success 0
+		cma_alloc_fail 0
+		unevictable_pgs_culled 27002
+		unevictable_pgs_scanned 0
+		unevictable_pgs_rescued 744
+		unevictable_pgs_mlocked 744
+		unevictable_pgs_munlocked 744
+		unevictable_pgs_cleared 0
+		unevictable_pgs_stranded 0
+		thp_fault_alloc 13
+		thp_fault_fallback 0
+		thp_fault_fallback_charge 0
+		thp_collapse_alloc 4
+		thp_collapse_alloc_failed 0
 		thp_file_alloc 0
+		thp_file_fallback 0
+		thp_file_fallback_charge 0
 		thp_file_mapped 0
-		thp_split_page 612
+		thp_split_page 0
 		thp_split_page_failed 0
-		thp_deferred_split_page 11238
-		thp_split_pmd 632
+		thp_deferred_split_page 1
+		thp_split_pmd 1
+		thp_scan_exceed_none_pte 0
+		thp_scan_exceed_swap_pte 0
+		thp_scan_exceed_share_pte 0
 		thp_split_pud 0
-		thp_zero_page_alloc 2
+		thp_zero_page_alloc 0
 		thp_zero_page_alloc_failed 0
-		thp_swpout 4
+		thp_swpout 0
 		thp_swpout_fallback 0
 		balloon_inflate 0
 		balloon_deflate 0
 		balloon_migrate 0
-		swap_ra 9661
-		swap_ra_hit 7872
+		swap_ra 0
+		swap_ra_hit 0
+		ksm_swpin_copy 0
+		cow_ksm 0
+		zswpin 0
+		zswpout 0
+		direct_map_level2_splits 29
+		direct_map_level3_splits 0
+		nr_unstable 0
 		EOF
 	fi
 }
