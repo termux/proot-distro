bbea792c22c44bfa49ced76047ab795a1fc4eb34
Leonid Pliushch <sylirre@termux.dev>
2023-03-29 15:12:52 +0300

    version 3.7.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -19,7 +19,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="3.6.1"
+PROGRAM_VERSION="3.7.0"
 
 #############################################################################
 ##
