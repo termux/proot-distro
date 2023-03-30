f9baaa7d69f9058aaff7b837b52a0dbe4a262a62
Leonid Pliushch <sylirre@termux.dev>
2023-03-30 12:52:42 +0300

    fedora: update plug-in

--- distro-plugins/fedora.sh
+++ distro-plugins/fedora.sh
@@ -4,7 +4,7 @@
 DISTRO_NAME="Fedora"
 DISTRO_COMMENT="Version 37. Supports only 64-bit CPUs."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.7.0/fedora-aarch64-pd-v3.7.0.tar.xz"
-TARBALL_SHA256['aarch64']="2cfcb80d716dd88da3ae774297f6da88ad70523ae9ab8975a0cefc75be4b09bb"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.7.0/fedora-x86_64-pd-v3.7.0.tar.xz"
-TARBALL_SHA256['x86_64']="08ba66f49de531299c2c116ab15fa776e4867764b8477207e912e875e21d658d"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.8.0/fedora-aarch64-pd-v3.8.0.tar.xz"
+TARBALL_SHA256['aarch64']="f36265d91ad50995d87c26fbc6cea6b3835e2f654f2535aef7de61a680e729e3"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.8.0/fedora-x86_64-pd-v3.8.0.tar.xz"
+TARBALL_SHA256['x86_64']="2e9d8b4cbe7f5096f7fdb186ef8a7a0efdab2dc809f086862769e8ff48a09609"
