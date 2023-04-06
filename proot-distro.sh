termux/proot-distro

commit b0bed093057b97320f23149820f9d267538c2cda

login: handle storage mounting differently
Issue https://github.com/termux/proot-distro/issues/285.

proot-distro.sh
@@ -1264,27 +1264,34 @@ command_login() {
 			fi
 			set -- "--bind=@TERMUX_HOME@" "$@"
 
-			# Setup bind mounting for shared storage.
-			# We want to use the primary shared storage mount point there
-			# with avoiding secondary and legacy mount points. As Android
-			# OS versions are different, some directories may be unavailable
-			# and we need to try them all.
-			if ls -1U /storage/self/primary/ > /dev/null 2>&1; then
-				set -- "--bind=/storage/self/primary:/sdcard" "$@"
-			elif ls -1U /storage/emulated/0/ > /dev/null 2>&1; then
-				set -- "--bind=/storage/emulated/0:/sdcard" "$@"
-			elif ls -1U /sdcard/ > /dev/null 2>&1; then
-				set -- "--bind=/sdcard:/sdcard" "$@"
-			else
-				# No access to shared storage.
-				:
-			fi
-
-			# /storage also optional bind mounting.
-			# If we can't access it, don't provide this directory
-			# in proot environment.
+			# Bind whole /storage directory when it is readable. This gives
+			# access to shared storage and on some Android versions to external
+			# disks such as SD cards. On failure try binding only shared
+			# storage.
 			if ls -1U /storage > /dev/null 2>&1; then
 				set -- "--bind=/storage" "$@"
+			else
+				# We want to use the primary shared storage mount point
+				# there with avoiding secondary and legacy mount points. As
+				# Android OS versions are different, some directories may
+				#be unavailable and we need to try them all.
+				local storage_path
+				if ls -1U /storage/self/primary/ > /dev/null 2>&1; then
+					storage_path="/storage/self/primary"
+				elif ls -1U /storage/emulated/0/ > /dev/null 2>&1; then
+					storage_path="/storage/emulated/0"
+				elif ls -1U /sdcard/ > /dev/null 2>&1; then
+					storage_path="/sdcard"
+				else
+					# Shared storage is not accessible.
+					storage_path=""
+				fi
+
+				if [ -n "$storage_path" ]; then
+					set -- "--bind=${storage_path}:/sdcard" "$@"
+					set -- "--bind=${storage_path}:/storage/emulated/0" "$@"
+					set -- "--bind=${storage_path}:/storage/self/primary" "$@"
+				fi
 			fi
 		fi
 

commit 0aacb11c395c0ca9229d5e546d8d077eed41d494

login: fix missing /sdcard directory
There was a regression caused by commit b0bed09 which results in
unavailable directory /sdcard when /storage is readable by Termux.

proot-distro.sh
@@ -1852,6 +1852,7 @@ command_login() {
 		# storage.
 		if ls -1U /storage > /dev/null 2>&1; then
 			set -- "--bind=/storage" "$@"
+			set -- "--bind=/storage/emulated/0:/sdcard" "$@"
 		else
 			# We want to use the primary shared storage mount point
 			# there with avoiding secondary and legacy mount points. As