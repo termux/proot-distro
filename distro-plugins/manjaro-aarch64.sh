c48c3df2600cf6efc2988de0689b89eb0bdb6f5f
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-04-15 05:21:36 +0600

    delete distros

--- distro-plugins/manjaro-aarch64.sh
+++
@@ -1,8 +0,0 @@
-# This is a default distribution plug-in.
-# Do not modify this file as your changes will be overwritten on next update.
-# If you want customize installation, please make a copy.
-DISTRO_NAME="Manjaro AArch64"
-DISTRO_COMMENT="Currently available only for AArch64."
-
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.5.1/manjaro-aarch64-pd-v3.5.1.tar.xz"
-TARBALL_SHA256['aarch64']="e887380e0ca412e5c81767f7991adf2c22da92408fff2d46a327577c4b99a383"
