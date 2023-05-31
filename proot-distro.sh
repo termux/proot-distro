6599a88a4081ca8c3c1bffb61cce0ddb6637a5dd
Leonid Pliushch <sylirre@termux.dev>
2023-05-31 13:37:09 +0300

    version 3.12.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -19,7 +19,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="3.11.0"
+PROGRAM_VERSION="3.12.0"
 
 #############################################################################
 ##
