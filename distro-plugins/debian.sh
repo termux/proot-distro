fe1ec444e5d3909ab02d003570bef24729624824
Leonid Pliushch <sylirre@termux.dev>
2023-06-17 11:44:40 +0300

    debian: update distribution plug-in

--- distro-plugins/debian.sh
+++ distro-plugins/debian.sh
@@ -2,13 +2,13 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Debian"
-DISTRO_COMMENT="A stable release (bullseye)."
+DISTRO_COMMENT="A stable release (bookworm)."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-aarch64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['aarch64']="dfce58c5f12c717bf26ff6b9aef92647916de7458c8ef486294f3df7c5700698"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-arm-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['arm']="a2f40207306b3bc2b8a01c2518270f68faa71538f9452b3e7f38794588620270"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-i686-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['i686']="11bb786b36ea0d1a0177f1eb8c2a8fea4916675dc1cf347a3556c61d89d7e54c"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-x86_64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['x86_64']="1957c76d4013d16f44eebfd9cc5b0074f421b5ea473e2a36c15d2939a802e339"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-aarch64-pd-v3.12.1.tar.xz"
+TARBALL_SHA256['aarch64']="3535e68ff47d148fac3e772630351171515b84200b73ca54608752338b21874e"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-arm-pd-v3.12.1.tar.xz"
+TARBALL_SHA256['arm']="4170bab1908ab88c22c764a53cc7546a154f829714ee240a25757bb8e5d40536"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-i686-pd-v3.12.1.tar.xz"
+TARBALL_SHA256['i686']="548f009afd88982cdf55e4bda1d30594add36e91425254b360613b270908cfed"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-x86_64-pd-v3.12.1.tar.xz"
+TARBALL_SHA256['x86_64']="00dd9cc73359500028e1268a6bf6819a33161ba48f7b110669b8b2db022f1322"
