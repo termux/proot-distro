6da1795b0525425d245ede8d598d838987171036
Leonid Pliushch <sylirre@termux.dev>
2023-07-08 01:45:18 +0300

    fedora: update distribution plug-in

--- distro-plugins/fedora.sh
+++ distro-plugins/fedora.sh
@@ -2,9 +2,9 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Fedora"
-DISTRO_COMMENT="Version 37. Supports only 64-bit CPUs."
+DISTRO_COMMENT="Version 38. Supports only 64-bit CPUs."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/fedora-aarch64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['aarch64']="d4bfd508695aea66548b44bb787b1575287609517ab42bc282788f87303410aa"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/fedora-x86_64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['x86_64']="e6386687148325a07f8822a2091a859e35661c79300360d498bf5f764ab22ed0"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.15.2/fedora-aarch64-pd-v3.15.2.tar.xz"
+TARBALL_SHA256['aarch64']="d6aa1a51f8d1f11a3388ed32a30643410345d5c8d22cb7c33e36cfa60942bbb1"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.15.2/fedora-x86_64-pd-v3.15.2.tar.xz"
+TARBALL_SHA256['x86_64']="167732ad9389523ca88ca9fdba470413322be15b3110845fa865e17b81e3ffaa"
