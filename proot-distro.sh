c9bba454439b3e6c256ca11d03e6b0507e7b9b0c
zongou <zongou@outlook.com>
2023-10-18 22:26:46 +0800

    Encode some of the fake procs with base64 and gzip
    Fake proc file stat and vmstat are now encoded.

--- proot-distro.sh
+++ proot-distro.sh
@@ -916,24 +916,14 @@ setup_fake_proc() {
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.stat" ]; then
-		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.stat"
-		cpu  1957 0 2877 93280 262 342 254 87 0 0
-		cpu0 31 0 226 12027 82 10 4 9 0 0
-		cpu1 45 0 664 11144 21 263 233 12 0 0
-		cpu2 494 0 537 11283 27 10 3 8 0 0
-		cpu3 359 0 234 11723 24 26 5 7 0 0
-		cpu4 295 0 268 11772 10 12 2 12 0 0
-		cpu5 270 0 251 11833 15 3 1 10 0 0
-		cpu6 430 0 520 11386 30 8 1 12 0 0
-		cpu7 30 0 172 12108 50 8 1 13 0 0
-		intr 127541 38 290 0 0 0 0 4 0 1 0 0 25329 258 0 5777 277 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-		ctxt 140223
-		btime 1680020856
-		processes 772
-		procs_running 2
-		procs_blocked 0
-		softirq 75663 0 5903 6 25375 10774 0 243 11685 0 21677
-		EOF
+ 		echo '
+H4sIAAAAAAAAA+2UYWrDMAyF/+cU7wiWZFn2aQbNshG2pV2SQo8/yWvKrjAoguCHPj/J+fHGyxWg
+poYErmZowtWPhSGZwZpRo5eG8XJNEAqOC4gTGyqDEjLaQRCy+rmUDCLKGUzuJWARv3JQjNyyCxVz
+jKv3LYwE9UAEouHKEk7GjrhZgeKxjesWw7jUQKzv4kP4zyR15xSMkjM1tlAfQ4HekYIsIZT9Nkkt
+cFkDebgYOkExgilV6J2QTszLvnrDNBOk+lbdu1e8kvC7gXDzb7xQzX80myE961nP+i81jPttB+XE
+LMNpn78mUKkpcapahst6HqdtmzZ4FnW1vazXZZmXdxz69HkeP6ZXt9rOb/u8fsO0lIgRbUlQIiZM
+PZ3MIjo4e1b5iB5zVMyGHzaioC2wBQAA
+   ' | base64 -d | gzip -d > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.stat"
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.uptime" ]; then
@@ -949,186 +939,29 @@ setup_fake_proc() {
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.vmstat" ]; then
-		cat <<- EOF > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.vmstat"
-		nr_free_pages 1743136
-		nr_zone_inactive_anon 179281
-		nr_zone_active_anon 7183
-		nr_zone_inactive_file 22858
-		nr_zone_active_file 51328
-		nr_zone_unevictable 642
-		nr_zone_write_pending 0
-		nr_mlock 0
-		nr_bounce 0
-		nr_zspages 0
-		nr_free_cma 0
-		numa_hit 1259626
-		numa_miss 0
-		numa_foreign 0
-		numa_interleave 720
-		numa_local 1259626
-		numa_other 0
-		nr_inactive_anon 179281
-		nr_active_anon 7183
-		nr_inactive_file 22858
-		nr_active_file 51328
-		nr_unevictable 642
-		nr_slab_reclaimable 8091
-		nr_slab_unreclaimable 7804
-		nr_isolated_anon 0
-		nr_isolated_file 0
-		workingset_nodes 0
-		workingset_refault_anon 0
-		workingset_refault_file 0
-		workingset_activate_anon 0
-		workingset_activate_file 0
-		workingset_restore_anon 0
-		workingset_restore_file 0
-		workingset_nodereclaim 0
-		nr_anon_pages 7723
-		nr_mapped 8905
-		nr_file_pages 253569
-		nr_dirty 0
-		nr_writeback 0
-		nr_writeback_temp 0
-		nr_shmem 178741
-		nr_shmem_hugepages 0
-		nr_shmem_pmdmapped 0
-		nr_file_hugepages 0
-		nr_file_pmdmapped 0
-		nr_anon_transparent_hugepages 1
-		nr_vmscan_write 0
-		nr_vmscan_immediate_reclaim 0
-		nr_dirtied 0
-		nr_written 0
-		nr_throttled_written 0
-		nr_kernel_misc_reclaimable 0
-		nr_foll_pin_acquired 0
-		nr_foll_pin_released 0
-		nr_kernel_stack 2780
-		nr_page_table_pages 344
-		nr_sec_page_table_pages 0
-		nr_swapcached 0
-		pgpromote_success 0
-		pgpromote_candidate 0
-		nr_dirty_threshold 356564
-		nr_dirty_background_threshold 178064
-		pgpgin 890508
-		pgpgout 0
-		pswpin 0
-		pswpout 0
-		pgalloc_dma 272
-		pgalloc_dma32 261
-		pgalloc_normal 1328079
-		pgalloc_movable 0
-		pgalloc_device 0
-		allocstall_dma 0
-		allocstall_dma32 0
-		allocstall_normal 0
-		allocstall_movable 0
-		allocstall_device 0
-		pgskip_dma 0
-		pgskip_dma32 0
-		pgskip_normal 0
-		pgskip_movable 0
-		pgskip_device 0
-		pgfree 3077011
-		pgactivate 0
-		pgdeactivate 0
-		pglazyfree 0
-		pgfault 176973
-		pgmajfault 488
-		pglazyfreed 0
-		pgrefill 0
-		pgreuse 19230
-		pgsteal_kswapd 0
-		pgsteal_direct 0
-		pgsteal_khugepaged 0
-		pgdemote_kswapd 0
-		pgdemote_direct 0
-		pgdemote_khugepaged 0
-		pgscan_kswapd 0
-		pgscan_direct 0
-		pgscan_khugepaged 0
-		pgscan_direct_throttle 0
-		pgscan_anon 0
-		pgscan_file 0
-		pgsteal_anon 0
-		pgsteal_file 0
-		zone_reclaim_failed 0
-		pginodesteal 0
-		slabs_scanned 0
-		kswapd_inodesteal 0
-		kswapd_low_wmark_hit_quickly 0
-		kswapd_high_wmark_hit_quickly 0
-		pageoutrun 0
-		pgrotated 0
-		drop_pagecache 0
-		drop_slab 0
-		oom_kill 0
-		numa_pte_updates 0
-		numa_huge_pte_updates 0
-		numa_hint_faults 0
-		numa_hint_faults_local 0
-		numa_pages_migrated 0
-		pgmigrate_success 0
-		pgmigrate_fail 0
-		thp_migration_success 0
-		thp_migration_fail 0
-		thp_migration_split 0
-		compact_migrate_scanned 0
-		compact_free_scanned 0
-		compact_isolated 0
-		compact_stall 0
-		compact_fail 0
-		compact_success 0
-		compact_daemon_wake 0
-		compact_daemon_migrate_scanned 0
-		compact_daemon_free_scanned 0
-		htlb_buddy_alloc_success 0
-		htlb_buddy_alloc_fail 0
-		cma_alloc_success 0
-		cma_alloc_fail 0
-		unevictable_pgs_culled 27002
-		unevictable_pgs_scanned 0
-		unevictable_pgs_rescued 744
-		unevictable_pgs_mlocked 744
-		unevictable_pgs_munlocked 744
-		unevictable_pgs_cleared 0
-		unevictable_pgs_stranded 0
-		thp_fault_alloc 13
-		thp_fault_fallback 0
-		thp_fault_fallback_charge 0
-		thp_collapse_alloc 4
-		thp_collapse_alloc_failed 0
-		thp_file_alloc 0
-		thp_file_fallback 0
-		thp_file_fallback_charge 0
-		thp_file_mapped 0
-		thp_split_page 0
-		thp_split_page_failed 0
-		thp_deferred_split_page 1
-		thp_split_pmd 1
-		thp_scan_exceed_none_pte 0
-		thp_scan_exceed_swap_pte 0
-		thp_scan_exceed_share_pte 0
-		thp_split_pud 0
-		thp_zero_page_alloc 0
-		thp_zero_page_alloc_failed 0
-		thp_swpout 0
-		thp_swpout_fallback 0
-		balloon_inflate 0
-		balloon_deflate 0
-		balloon_migrate 0
-		swap_ra 0
-		swap_ra_hit 0
-		ksm_swpin_copy 0
-		cow_ksm 0
-		zswpin 0
-		zswpout 0
-		direct_map_level2_splits 29
-		direct_map_level3_splits 0
-		nr_unstable 0
-		EOF
+  		echo '
+H4sIAAAAAAAAA31X27qrKAy+30/RR/DQVvs0fFSpMkVwENu9+vSTBFCsrrkzfxJyIAfUlj2sEGzk
+nZhOeXUu8/L6R1v2MVowqXnj5Eswro0G7q2o84WZsqq8LvdaD6nEqSjqS/2tRJxLXhYrZ9biJRvH
+78C5nosFf1vpwD+hW6m7U4b4oEzz9J93M+tG+O/P5KMggqJqBo7UPHDWS3fKi8vtWlw9MMhpisyH
+sUJ2OpJSO2GV4C9xqoqAgUmuticY1wvrrf2WqKMc/ZKew8wcJGVS/M6saBSXA+F1dssXxqxTVlVn
+ZzI5GcWdaL0j2QYig9mft7FPSPAkHNOmpSwmkBUPPisX9Q84+2MoIDBxoLSw9lpWTA5u49CS5xw7
+HML2waFyKOmqKijrAx9H0Z7qW3ah8oBDgkRxKS/XG4KttO7Hn0BVd+exzBaSOTGMHpv6QQxw13V1
+zhea9XMnkjL04Di0wX62GP8S9A5t5SgMZ7mGwrZCu0SHLL6GqeHaO+c1AiKHQbQSE7xJC8Yn4+Go
+5USoBtdb45yCctjAT2G1UNgqzabkvMdGKTZKDbf57yztElyErYAWmiIcjpoc5rSAwkQQY2FU3eEu
+yjPV6ySaPc/n883Hhjc9HTt2ozWDgTCnuWkE9fOKQSJa2fKYGrpcDFRMvVHtCS79cj2vHLzdzsI4
+aRMhuN0MhODQTmoqnqwmyswOjU1viDR8BKjjCmYFg4uEMIuULotTcc0XRBs74EiBVs+q2wIP5hVy
+vKjiDECASMggJLilybYF4PwNFAxssPX0VDcaGLvpKcdw+ErQwYFcDg106q6XXw/DGXwqs6rKcgo7
+dD3xWrEhFf/8kDjp4USB3F9vVQnkwP/xyLmuE1FfATB/pFLhc57EKb8VJTnjBFfsiQXjJT0Aly0a
+lwDP2FRt8IuKJ9ELSKIYZbaa1HmpPaRTc8Q/0PEySw+ujDADAxXmXvR7ZRIZuLQyQ6uyBwfQG5I0
+1FESSNwUE8NDNbG902wjEzBl3uw9cPvEBcqgz5un+lnZvez6Qz6GCA1hZ+8jRIbLBr5ba0ZqaOri
+CKBH8G3MwJ7+OmnDjpDlecQeXlY15u8Qh6XNqEyOoLC+47E4T2CqdTY4BTXmic0giRimEQDXj0FH
+wlheBbf4sfCoJBZBY4YRyp4t1pYriBx6tuzhuK0TiFo31fSGF/biX0RaDmUL24I/xR793aMg8OVY
+79Sd3ee2/WF+SK32dqzoGWT+W3bFglDy4GFQ2qyZFZZwUWVZsWOu7nxzYH43M3Aq2CffPHo9/sab
+9f9wG1ho9tDehFu6JRbefHgsYWAw3xPoAVh4VexB1vTcdiLwGtijfJxEOOZ8AK4NTofhC8ILJ8C3
+xRTbGiTW8vxAhMqWumUHbE234iEsZCbVyFONoY00TjLxt4HxDatEUyvHwxMWTpffWODzRs1bmKMv
+H2GNdzHNxRe6dX9Z3iuR5u2OKtACUj+U31cRgbi/kNBHOGQxBMvXL/oFwck5MHo2wGWOP9Rob1gb
++ET7xOfEZ/Eo7Aa4FqbES6jCxwuv1tuOV0Ze5n8dJhc283+z/Bhw5Q0AAA==
+' | base64 -d | gzip -d > "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.vmstat"
 	fi
 
 	if [ ! -f "${INSTALLED_ROOTFS_DIR}/${distro_name}/proc/.sysctl_entry_cap_last_cap" ]; then
