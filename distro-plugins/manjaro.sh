373f98298ee9b474fa55eae6bdcedd0eb2c7c8e4
Leonid Pliushch <sylirre@termux.dev>
2023-10-25 20:36:10 +0300

    manjaro: ensure that /etc/environment always read
    Adjust PAM configuration.

--- distro-plugins/manjaro.sh
+++ distro-plugins/manjaro.sh
@@ -6,3 +6,11 @@ DISTRO_COMMENT="Currently available only for AArch64."
 
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/manjaro-aarch64-pd-v3.18.1.tar.xz"
 TARBALL_SHA256['aarch64']="6d6595ab9650a1836efe9dd23e0b7b321cdaf01b15ed95413036f53f6a25f495"
+
+distro_setup() {
+	# Fix environment variables on login or su.
+	local f
+	for f in su su-l system-local-login system-remote-login; do
+		echo "session  required  pam_env.so readenv=1" >> ./etc/pam.d/"${f}"
+	done
+}
