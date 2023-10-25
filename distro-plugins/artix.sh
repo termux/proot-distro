432cd77b99ad8d91849bc505dc9abedd4e25f7c6
Leonid Pliushch <sylirre@termux.dev>
2023-10-25 13:46:28 +0300

    artix: add distribution plug-in

---
+++ distro-plugins/artix.sh
@@ -0,0 +1,8 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Artix Linux"
+DISTRO_COMMENT="Currently available only for AArch64."
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/artix-aarch64-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['aarch64']="9801031864da6bc9dc69293695797f8aba7866c273bb7403f95c4e70be987936"
