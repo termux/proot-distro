bd0b6c201d50b118021601d9481a2e37e800d5c6
Leonid Pliushch <sylirre@termux.dev>
2023-10-25 21:17:10 +0300

    version 4.0.0
    Major change 1: replaced su login method with internal one
    
    PRoot Distro will no longer use /etc/profile.d/termux-proot.sh for
    setting up environment variables. This method has number of flaws, e.g.
    this file may not be sourced by Zsh or other alternative shells.
    
    Now login being handled entirely by PRoot Distro. As side effect,
    now users who have `proot` buggy on their devices and have su
    throwing errors like "function not implemented" should be able to
    log in.
    
    Environment variables are handled by PRoot Distro for initial login
    and by PAM (/etc/environment) when switching users via su/sudo.
    
    Major change 2: proot-distro now can be called as pd for less typing
    
    Examples:
    
      pd install ubuntu
      pd login ubuntu

--- proot-distro.sh
+++ proot-distro.sh
@@ -20,7 +20,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="3.18.1"
+PROGRAM_VERSION="4.0.0"
 
 #############################################################################
 ##
