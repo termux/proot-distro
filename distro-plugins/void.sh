ee701242d85137c7371fb98cdfc26294d66be984
Leonid Pliushch <sylirre@termux.dev>
2023-10-25 16:03:36 +0300

    void: update plug-in

--- distro-plugins/void.sh
+++ distro-plugins/void.sh
@@ -3,14 +3,14 @@
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Void Linux"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.11.0/void-aarch64-pd-v3.11.0.tar.xz"
-TARBALL_SHA256['aarch64']="879b4ddced8cb8723193c88b53cbafdd5bf8bd6f0f1fb6445c3a8a184329f105"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.11.0/void-arm-pd-v3.11.0.tar.xz"
-TARBALL_SHA256['arm']="b372b38eef61da991efa423fa31c644e8a156d7f15c438a492c37d68859a21a5"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.11.0/void-i686-pd-v3.11.0.tar.xz"
-TARBALL_SHA256['i686']="5d90191fce3968cd4993f3c6a361c5e20ec21aa149adc113ada527a8132a3a2c"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.11.0/void-x86_64-pd-v3.11.0.tar.xz"
-TARBALL_SHA256['x86_64']="676546678dc2fdfc4e5ffa5c3dc8b3aa5dc8b884108fdf2346f4edd0311679bc"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/void-aarch64-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['aarch64']="a2f17acfc49e6f1a6f2d121cb6f69e5d64ff3bc642364939acd5eba5738c4467"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v3.18.1/void-arm-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['arm']="aa2d01e8e383ea65374ae70f6ad47e63ea93ab44c4c6580cb138f7fae354dbae"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.18.1/void-i686-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['i686']="5dd0d58ee4abde72e68137c10d5b0a6866a9ec2d88d9f834bc56864cfc96c662"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/void-x86_64-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['x86_64']="c08229efd5a7dfca6f496e774d52a9c8cbbbb3f19189c2482343872159ec6325"
 
 distro_setup() {
 	# Set default shell to bash.
