9abb4d908e50cc8f843d7921c1666428b377554d
Leonid Pliushch <sylirre@termux.dev>
2023-12-11 01:20:52 +0200

    debian: update plug-in

--- distro-plugins/debian.sh
+++ distro-plugins/debian.sh
@@ -4,18 +4,18 @@
 DISTRO_NAME="Debian"
 DISTRO_COMMENT="A stable release (bookworm)."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.0.2/debian-aarch64-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['aarch64']="5100c435d4f410e4bce0d448e273a3783a9a94e31bfb86f1e1448f7e696df87a"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.0.2/debian-arm-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['arm']="5fd0cc8a3b96486658222569a8d250f1ed1ae3bc2c54cff4c191b7625264ab1a"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.0.2/debian-i686-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['i686']="6a3cdbf0a19251f932d256896ede242dddb2fec0e669f5cabcb1d21942745a0d"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.0.2/debian-x86_64-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['x86_64']="ed26084174fc6c000817276f3fa36594327c5df2f8f7ba98007474d64857b669"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.5.0/debian-aarch64-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['aarch64']="bd675876f420c1c774649360041d7c4c268dc37d587afedfa6abe37580088cfa"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.5.0/debian-arm-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['arm']="579cd4b32eea89729e9231783a919df667fe28606c413b7550889c8c8968fa41"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.5.0/debian-i686-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['i686']="8309a33dea0c2002154aa9b62845ed376ac443fdc73c0f979dfc5d657a90c3df"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.5.0/debian-x86_64-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['x86_64']="c5320c114a1e9d4caa86642f9b9f4c3933788f0382250cfbad9e03bd57a9337d"
 
 distro_setup() {
 	# Configure en_US.UTF-8 locale.
-	run_proot_cmd sed -i -E 's/# (en_US.UTF-8)/\1/g' /etc/locale.gen
+	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 }
 
