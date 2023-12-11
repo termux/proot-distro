7021a4ab324127663a9d2a7bd740572fb6f3b8c1
Leonid Pliushch <sylirre@termux.dev>
2023-12-11 02:22:43 +0200

    ubuntu: update plug-in

--- distro-plugins/ubuntu.sh
+++ distro-plugins/ubuntu.sh
@@ -4,15 +4,15 @@
 DISTRO_NAME="Ubuntu"
 DISTRO_COMMENT="Standard release (mantic). Not available for x86 32-bit (i686) CPUs."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.0.2/ubuntu-aarch64-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['aarch64']="257e71bbbb8f336491f63a1d1927a83584d8b4ff8a7f4fb15392674473b838d2"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.0.2/ubuntu-arm-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['arm']="aa72f2a1bbb9d55e9b6b239d539183990e8ba6b2fcd038f5cb5680e6326b17b6"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.0.2/ubuntu-x86_64-pd-v4.0.2.tar.xz"
-TARBALL_SHA256['x86_64']="c9e5e35dafc3dfaf915259025f1f1c58aad1fce9c45396e2f4da44d578c4be19"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.5.0/ubuntu-aarch64-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['aarch64']="78f6e74531c6e7c61d6b90ea9b7e25738c14f2f4f2bb07cb1614414ef06017d5"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.5.0/ubuntu-arm-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['arm']="3af5171713631aa2f672602f0099d22e242a9fe19df910924dbf5e93497496c8"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.5.0/ubuntu-x86_64-pd-v4.5.0.tar.xz"
+TARBALL_SHA256['x86_64']="5a5212f5389fb805030c4db2dcde99590136291bb9f44aad62b4665b66f66669"
 
 distro_setup() {
 	# Configure en_US.UTF-8 locale.
-	run_proot_cmd sed -i -E 's/# (en_US.UTF-8)/\1/g' /etc/locale.gen
+	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 }
