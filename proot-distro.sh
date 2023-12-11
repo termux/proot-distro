a7ea71a915de15f1a83f15523e9003022e3eafd6
Leonid Pliushch <sylirre@termux.dev>
2023-12-11 02:25:41 +0200

    version 4.6.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -20,7 +20,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="4.5.0"
+PROGRAM_VERSION="4.6.0"
 
 #############################################################################
 ##
