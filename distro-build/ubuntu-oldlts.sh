3e37b8401898d593a5cd8ced1d6135dc85f9663d
Sylirre <sylirre@termux.dev>
2024-01-31 11:25:49 +0200

    ubuntu-oldlts: set right output plugin file name

--- distro-build/ubuntu-oldlts.sh
+++ distro-build/ubuntu-oldlts.sh
@@ -23,7 +23,7 @@ bootstrap_distribution() {
 }
 
 write_plugin() {
-	cat <<- EOF > "${PLUGIN_DIR}/ubuntu.sh"
+	cat <<- EOF > "${PLUGIN_DIR}/ubuntu-oldlts.sh"
 	# This is a default distribution plug-in.
 	# Do not modify this file as your changes will be overwritten on next update.
 	# If you want customize installation, please make a copy.
