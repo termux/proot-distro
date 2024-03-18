41a18d8b02ade010b63d72546624d668f9d47fd3
Sylirre <sylirre@termux.dev>
2024-03-18 16:39:12 +0200

    openkylin: update to 'nile'

--- distro-plugins/openkylin.sh
+++ distro-plugins/openkylin.sh
@@ -2,9 +2,9 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="OpenKylin"
-DISTRO_COMMENT="Version 'yangtze'. Supports only 64-bit CPUs."
+DISTRO_COMMENT="Version 'nile'. Supports only 64-bit CPUs."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/openkylin-aarch64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['aarch64']="ba869dec713d981a61c387115e93dfe3ca0c0958ccc398efefd174ddcb3d3a8d"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/openkylin-x86_64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['x86_64']="03f43ab9a38ae44ffee06ca6c950a60814c00d8f202d487e95858987f6ee6cbe"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.10.0/openkylin-aarch64-pd-v4.10.0.tar.xz"
+TARBALL_SHA256['aarch64']="c6cac0682228739406804fba62ca0e87f9d8a77f8355baef2c30758fc38a536f"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.10.0/openkylin-x86_64-pd-v4.10.0.tar.xz"
+TARBALL_SHA256['x86_64']="d3505c0301636850a9fd018349808b1c0735770d222db99b2eda53602fe8cc17"
