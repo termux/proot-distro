70169410dba1aa9530ed5c02548a85987e9a6b31
Sylirre <sylirre@termux.dev>
2024-07-07 13:25:44 +0300

    fedora: version 40

--- distro-build/fedora.sh
+++ distro-build/fedora.sh
@@ -1,23 +1,25 @@
 dist_name="Fedora"
-dist_version="39-1.5"
+dist_version="40-1.14"
 
 bootstrap_distribution() {
 	sudo rm -f "${ROOTFS_DIR}"/fedora-*.tar.xz
 
 	for arch in aarch64 x86_64; do
 		curl --fail --location \
-			--output "${WORKDIR}/fedora-${dist_version}-${arch}.tar.xz" \
-			"https://mirror.de.leaseweb.net/fedora/linux/releases/${dist_version:0:2}/Container/${arch}/images/Fedora-Container-Base-${dist_version}.${arch}.tar.xz"
-
+			--output "${WORKDIR}/Fedora-Container-Base-Generic.${arch}-${dist_version}.oci.tar.xz" \
+			"https://mirror.de.leaseweb.net/fedora/linux/releases/${dist_version%%-*}/Container/${arch}/images/Fedora-Container-Base-Generic.${arch}-${dist_version}.oci.tar.xz"
 		sudo rm -rf "${WORKDIR}/fedora-tmp" "${WORKDIR}/fedora-$(translate_arch "$arch")"
 		mkdir "${WORKDIR}/fedora-tmp"
+		tar -C "${WORKDIR}/fedora-tmp" -Jxf "${WORKDIR}/Fedora-Container-Base-Generic.${arch}-${dist_version}.oci.tar.xz"
+		oci_manifest=$(jq -r '.manifests[0].digest' "${WORKDIR}/fedora-tmp"/index.json | cut -d ':' -f 2)
+		oci_layers=$(jq -r '.layers[].digest' "${WORKDIR}/fedora-tmp/blobs/sha256/${oci_manifest}" | cut -d ':' -f 2)
+
 		sudo mkdir -m 755 "${WORKDIR}/fedora-$(translate_arch "$arch")"
-		sudo tar -Jx --strip-components=1 \
-			-f "${WORKDIR}/fedora-${dist_version}-${arch}.tar.xz" \
-			-C "${WORKDIR}/fedora-tmp"
-		sudo tar -xp --acls --xattrs --xattrs-include='*' \
-			-f "${WORKDIR}/fedora-tmp"/layer.tar \
-			-C "${WORKDIR}/fedora-$(translate_arch "$arch")"
+		for layer in ${oci_layers}; do
+			sudo tar -zxp --acls --xattrs --xattrs-include='*' \
+				-f "${WORKDIR}/fedora-tmp/blobs/sha256/${layer}" \
+				-C "${WORKDIR}/fedora-$(translate_arch "$arch")"
+		done
 		sudo rm -rf "${WORKDIR}/fedora-tmp"
 
 		cat <<- EOF | sudo unshare -mpf bash -e -
@@ -45,7 +47,7 @@ write_plugin() {
 	# Do not modify this file as your changes will be overwritten on next update.
 	# If you want customize installation, please make a copy.
 	DISTRO_NAME="Fedora"
-	DISTRO_COMMENT="Version ${dist_version:0:2}. Supports only 64-bit CPUs."
+	DISTRO_COMMENT="Version ${dist_version%%-*}. Supports only 64-bit CPUs."
 
 	TARBALL_URL['aarch64']="${GIT_RELEASE_URL}/fedora-aarch64-pd-${CURRENT_VERSION}.tar.xz"
 	TARBALL_SHA256['aarch64']="$(sha256sum "${ROOTFS_DIR}/fedora-aarch64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
