e7ce12377c61a9c38f4e01c530286782c950112f
Sylirre <sylirre@termux.dev>
2025-01-10 01:22:01 +0200

    update rootfs: opensuse, pardus

--- distro-plugins/opensuse.sh
+++ distro-plugins/opensuse.sh
@@ -1,17 +1,17 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
-DISTRO_NAME="openSUSE"
+DISTRO_NAME="OpenSUSE"
 DISTRO_COMMENT="Rolling release (Tumbleweed)."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/opensuse-aarch64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['aarch64']="cf81e80a7116f1a4638ba37619c4daf9b1052cc9635fe2925b26b9b60508600d"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.6.0/opensuse-arm-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['arm']="e191db4043949f550585fc9fa2912229384a64b61976ee937364a460dc826b68"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.6.0/opensuse-i686-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['i686']="e497ebea3d92c676a320f48ed35146d564160791dbfa7bc677f0959bb3a40248"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/opensuse-x86_64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['x86_64']="8cdfc2965983cfae03ae05f46eb68771cb7f19699c85bd9fb9f453cee5c7cb50"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-aarch64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['aarch64']="af234643447fd86ac90a9e380501f202eb131046cc6aa3a97807b40bdafcfcf2"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-arm-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['arm']="1c569c685626789856786147263043cadc29380d94f11d7d837b8d569083d91b"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-i686-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['i686']="95dee97a4032b424b3445823f0bcf985d792d3ae56d525a902c8dd72589553a3"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-x86_64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['x86_64']="9b3f8e452cf20f23dffd424c4d55942a1fb40657e98c2e2fa50ac7af59e4c9bd"
 
 distro_setup() {
 	# Lock package filesystem to remove issues regarding zypper dup
--- distro-plugins/pardus.sh
+++ distro-plugins/pardus.sh
@@ -2,11 +2,11 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Pardus"
-DISTRO_COMMENT="Version 'yirmibir'."
+DISTRO_COMMENT="Version 'yirmiuc'."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/pardus-aarch64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['aarch64']="a549bed5384d13ee8ab353d64c2ed8ce1d311d0b2612c5a1cb9fe664f0650a37"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.6.0/pardus-i686-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['i686']="e3b7c6e1eca99cbaa19f9a099c4c851ebb70852d444c0f30c7e6140c6260c9e3"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/pardus-x86_64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['x86_64']="d5e5b4a28dea46630d9308233bfa345785e28899fbda9672a1655224f3f9f0bf"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/pardus-aarch64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['aarch64']="b4dccbb4a3f8263fdbae45a73c685bf27d9ab2819f17d4939c9988f759f88d3b"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.18.0/pardus-i686-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['i686']="271fd6b6e80f840566bbf5dda0fca5f4d35b24e5c679d895bc4c35422c8bee06"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/pardus-x86_64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['x86_64']="05fb46e45501d983774171c98423d46ae2f1a191332ba1b83eb33a99a5a828e2"
