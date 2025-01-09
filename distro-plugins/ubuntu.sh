fb1a59959ad23000d5bfe7b8b5879aee987a6e1d
Sylirre <sylirre@termux.dev>
2025-01-10 01:46:27 +0200

    update plugin for ubuntu distribution

--- distro-plugins/ubuntu.sh
+++ distro-plugins/ubuntu.sh
@@ -4,15 +4,31 @@
 DISTRO_NAME="Ubuntu (24.04)"
 DISTRO_COMMENT="LTS release (noble)."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/ubuntu-noble-aarch64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['aarch64']="81ac0fb0d16ded12ab11cede62f67b875ff56f9fa1aa9eb786415c3ec5c477d2"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.17.3/ubuntu-noble-arm-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['arm']="611f39e8b942202d14608026ef3d674b35a1fea6e780dbaa5ca001cbb63d04c0"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/ubuntu-noble-x86_64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['x86_64']="1680d024bf47d2414a36759af528ca3569a73b70682daa3d9693ba63157cb5a8"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/ubuntu-noble-aarch64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['aarch64']="91acaa786b8e2fbba56a9fd0f8a1188cee482b5c7baeed707b29ddaa9a294daa"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.18.0/ubuntu-noble-arm-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['arm']="2afb7e1ff17983fa2cf4c57edeea6be427ffb0359d8628b24a147b4c8aa276d5"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/ubuntu-noble-x86_64-pd-v4.18.0.tar.xz"
+TARBALL_SHA256['x86_64']="3a769bce23985effb504140d43b6ddd73dac9e261d1932894afa31de81e45414"
 
 distro_setup() {
 	# Configure en_US.UTF-8 locale.
 	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+
+	# Configure Firefox PPA.
+	add-apt-repository ppa:mozillateam/firefox-next
+	cat <<- CONFIG_EOF > ./etc/apt/preferences.d/pin-mozilla-ppa
+	Package: *
+	Pin: release o=LP-PPA-mozillateam-firefox-next
+	Pin-Priority: 9999
+	CONFIG_EOF
+
+	# Configure Thunderbird PPA.
+	add-apt-repository ppa:mozillateam/thunderbird-next
+	cat <<- CONFIG_EOF > ./etc/apt/preferences.d/pin-thunderbird-ppa
+	Package: *
+	Pin: release o=LP-PPA-mozillateam-thunderbird-next
+	Pin-Priority: 9999
+	CONFIG_EOF
 }
