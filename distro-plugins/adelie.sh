c36fbd06fad325937f720b580355ded88ecbd58f
Sylirre <sylirre@termux.dev>
2025-01-22 21:51:55 +0200

    adelie: add distribution plugin

---
+++ distro-plugins/adelie.sh
@@ -0,0 +1,12 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Adélie Linux"
+DISTRO_COMMENT="Version '1.0-beta6'."
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.19.0/adelie-aarch64-pd-v4.19.0.tar.xz"
+TARBALL_SHA256['aarch64']="b573ad9f6a3f6ef72d32a40378aed011ea5828e3f0a99ad9b7b0dabcb9f35028"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.19.0/adelie-arm-pd-v4.19.0.tar.xz"
+TARBALL_SHA256['arm']="14eefda8e688c54dd824f70a9f3afc11d292247447f6c236751d5afd5f297a9e"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.19.0/adelie-x86_64-pd-v4.19.0.tar.xz"
+TARBALL_SHA256['x86_64']="c8a4318992f042c60e691c4fb775bd276a9159834b429104a270e39f6db9708a"
