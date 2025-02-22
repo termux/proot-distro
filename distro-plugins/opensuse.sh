d96d8e526e194f0bb1235e386e020c8f153cdd7c
Sylirre <sylirre@termux.dev>
2025-02-22 20:48:06 +0200

    update opensuse plugin

--- distro-plugins/opensuse.sh
+++ distro-plugins/opensuse.sh
@@ -4,14 +4,14 @@
 DISTRO_NAME="OpenSUSE"
 DISTRO_COMMENT="Rolling release (Tumbleweed)."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-aarch64-pd-v4.18.0.tar.xz"
-TARBALL_SHA256['aarch64']="af234643447fd86ac90a9e380501f202eb131046cc6aa3a97807b40bdafcfcf2"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-arm-pd-v4.18.0.tar.xz"
-TARBALL_SHA256['arm']="1c569c685626789856786147263043cadc29380d94f11d7d837b8d569083d91b"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-i686-pd-v4.18.0.tar.xz"
-TARBALL_SHA256['i686']="95dee97a4032b424b3445823f0bcf985d792d3ae56d525a902c8dd72589553a3"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.18.0/opensuse-x86_64-pd-v4.18.0.tar.xz"
-TARBALL_SHA256['x86_64']="9b3f8e452cf20f23dffd424c4d55942a1fb40657e98c2e2fa50ac7af59e4c9bd"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.21.0/opensuse-aarch64-pd-v4.21.0.tar.xz"
+TARBALL_SHA256['aarch64']="23cdd0ba0f85e261da2560e3fcf7ac19cd1bcf7afdc2144b694ea9229058fa2a"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.21.0/opensuse-arm-pd-v4.21.0.tar.xz"
+TARBALL_SHA256['arm']="a02caa5a17fd90399d5864772eb1317727abe0ffe5b088cf496e496e0f33fc76"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.21.0/opensuse-i686-pd-v4.21.0.tar.xz"
+TARBALL_SHA256['i686']="e7e2f24928bd44dd270cd503257697517b1284ae57ea18aa5a6f66cd1857d5fa"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.21.0/opensuse-x86_64-pd-v4.21.0.tar.xz"
+TARBALL_SHA256['x86_64']="cfb70fe5acd74928f5573a59ac038eff91f1bd4c6a95cc00e69424e6f3ae89b3"
 
 distro_setup() {
 	# Lock package filesystem to remove issues regarding zypper dup
