6023004c493f9416801c8314d94fe60b21314f33
Sylirre <sylirre@termux.dev>
2025-04-20 19:35:51 +0300

    fedora: update plugin script

--- distro-plugins/fedora.sh
+++ distro-plugins/fedora.sh
@@ -2,12 +2,12 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Fedora"
-DISTRO_COMMENT="Version 41."
+DISTRO_COMMENT="Version 42."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.23.0/fedora-aarch64-pd-v4.23.0.tar.xz"
-TARBALL_SHA256['aarch64']="837ccdbd862c96bf7140c0c21f32f2b89b7c910cb5760e6d5946c425c640524c"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.23.0/fedora-x86_64-pd-v4.23.0.tar.xz"
-TARBALL_SHA256['x86_64']="db2fd4267fa38b1dc10178b8e9e8e0f6d5a100360b8e2ba2d7fd4916225619fd"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.24.0/fedora-aarch64-pd-v4.24.0.tar.xz"
+TARBALL_SHA256['aarch64']="48abf1d8b9cc7625d4212cc604ce3c113ea6d6d806de60b2c3f74c5b5452cd72"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.24.0/fedora-x86_64-pd-v4.24.0.tar.xz"
+TARBALL_SHA256['x86_64']="105ffd9a7d989ac09ee3adb7c4f00b72a3cc997c1e3fd99599bf16578dd8e20c"
 
 distro_setup() {
 	# Fix environment variables on login or su.
