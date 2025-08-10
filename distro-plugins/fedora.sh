8b8339af0742210f201211757507031d4a06661e
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-08-10 23:28:51 +0300

    fedora: update plugin script

--- distro-plugins/fedora.sh
+++ distro-plugins/fedora.sh
@@ -4,10 +4,10 @@
 DISTRO_NAME="Fedora"
 DISTRO_COMMENT="Version 42. Broken on Android 15+."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.25.0/fedora-aarch64-pd-v4.25.0.tar.xz"
-TARBALL_SHA256['aarch64']="9ca78013e8d51d634c0906e389e6174a816487491534385df01bee4752a5a9d8"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.25.0/fedora-x86_64-pd-v4.25.0.tar.xz"
-TARBALL_SHA256['x86_64']="b1a64e2bd37947cc8364ab7030409775748c666d2c2c4d5f331cddba653f2a97"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.27.0/fedora-aarch64-pd-v4.27.0.tar.xz"
+TARBALL_SHA256['aarch64']="55409069cbe314cb8e42f9509a89eb865b81a2010be88aa1ead27db29dbf03ee"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.27.0/fedora-x86_64-pd-v4.27.0.tar.xz"
+TARBALL_SHA256['x86_64']="84389fcbf6a1aea6ab52a1b3909bfd84293b58937afacd6150164757558ad2b3"
 
 distro_setup() {
 	# Fix environment variables on login or su.
