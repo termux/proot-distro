9985c358cff3b1b51965e9633fc7521cf6f5562d
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-08-10 23:32:38 +0300

    version 4.28.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -21,7 +21,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="4.27.0"
+PROGRAM_VERSION="4.28.0"
 
 #############################################################################
 #
