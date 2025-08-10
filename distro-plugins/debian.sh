5e3a3c4432b97c2cfc659ad2bd30d336491cfe06
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-08-10 17:56:46 +0300

    debian: update plugin script

--- distro-plugins/debian.sh
+++ distro-plugins/debian.sh
@@ -1,17 +1,17 @@
 # This is a default distribution plug-in.
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
-DISTRO_NAME="Debian (bookworm)"
+DISTRO_NAME="Debian (trixie)"
 DISTRO_COMMENT="Stable release."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-aarch64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['aarch64']="3a841a794ae5999b33e33b329582ed0379d4f54ca62c6ce5a8eb9cff5ef8900b"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-arm-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['arm']="85861ab139d4042302796cf46a93a9efbcb4808c06f7a1ae5fb71812f4564424"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-i686-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['i686']="1fb3a6b0ea679e3797b35984049abf22bfe3b6ab79e9bb98cdfc54994712e1e4"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-x86_64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['x86_64']="675e534333adcbf369e97abda3088927651e5d91612ae5727c52ff2284f4b8c8"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-aarch64-pd-v4.26.0.tar.xz"
+TARBALL_SHA256['aarch64']="cda75346f2c9e09e8a802665745b5a7e2bd6d8584dbf1c86c8c57ef54c4e2d3c"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-arm-pd-v4.26.0.tar.xz"
+TARBALL_SHA256['arm']="868ad59b44098d7175819bdabda0dcd98ddc67af15c153cc9e22797ac77f9dd3"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-i686-pd-v4.26.0.tar.xz"
+TARBALL_SHA256['i686']="8579087c23d759f3ded2c88d8eb707fe9efe524330c8894828f3246cdcc36117"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-x86_64-pd-v4.26.0.tar.xz"
+TARBALL_SHA256['x86_64']="e2edc15363395936cf0cba8c440a108458dba58fb496d3d962909d7a8d9777ae"
 
 distro_setup() {
 	# Configure en_US.UTF-8 locale.
