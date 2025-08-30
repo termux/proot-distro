b8bb9b11f74fe9f408d0f1ca9504270d99f7f968
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-08-31 00:29:15 +0300

    rockylinux: update plugin script

--- distro-plugins/rockylinux.sh
+++ distro-plugins/rockylinux.sh
@@ -4,7 +4,7 @@
 DISTRO_NAME="Rocky Linux"
 DISTRO_COMMENT="Version 9.5"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.20.0/rocky-aarch64-pd-v4.20.0.tar.xz"
-TARBALL_SHA256['aarch64']="687ba6987186e34db6330edba13f419c0afe3fea602cd9ce7a6a5dafa423740c"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.20.0/rocky-x86_64-pd-v4.20.0.tar.xz"
-TARBALL_SHA256['x86_64']="b3fc30dd204f893cec3871d2077d3ac03eaf6504f803a298baba1b4bb23481a1"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.28.0/rocky-aarch64-pd-v4.28.0.tar.xz"
+TARBALL_SHA256['aarch64']="554d6b3910ea3e88d3da6ff258c76cd6298185082abdb325ef2503be1761aacf"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.28.0/rocky-x86_64-pd-v4.28.0.tar.xz"
+TARBALL_SHA256['x86_64']="6fe57906b1d2248e86f3d1a63c8126bca4224339ed5000b6dccb8fc828f2f8d3"
