e0f1194fc7c36d48e07a0c94b2340babaa0e55b6
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-09-27 19:52:17 +0300

    update plugins for adelie, alpine, chimera, debian

--- distro-plugins/chimera.sh
+++ distro-plugins/chimera.sh
@@ -2,11 +2,11 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Chimera Linux"
-DISTRO_COMMENT="Version '20241204'."
+DISTRO_COMMENT="Version '20250420'."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/chimera-aarch64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['aarch64']="e0fdd16e9e941db33179e15e0f28994a364dbca6f8e3b65ec037e24b9480aa84"
-TARBALL_URL['riscv64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/chimera-riscv64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['riscv64']="2664534b873cd3072a5432251619e90b866ec4c96325ea6494c7aca396b4d97e"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/chimera-x86_64-pd-v4.17.3.tar.xz"
-TARBALL_SHA256['x86_64']="e3f97eeea9f40084301ea0c6cdb666685173e1efaa4fabd4ba74b0d4ad37d83a"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/chimera-aarch64-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['aarch64']="cd27f3d8521a510fcb73f3cfa8759a09422a7bc33c30a8cd1659233e318dbb47"
+TARBALL_URL['riscv64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/chimera-riscv64-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['riscv64']="3790528f050b760041fc653be85b6b29045e702624979a103c9ddd0482a74e96"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/chimera-x86_64-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['x86_64']="3efe628d5c57d4d35ac848d7631f9567277e32f59c3f0e4a790d428b323c1a37"
--- distro-plugins/debian.sh
+++ distro-plugins/debian.sh
@@ -4,14 +4,14 @@
 DISTRO_NAME="Debian (trixie)"
 DISTRO_COMMENT="Stable release."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-aarch64-pd-v4.26.0.tar.xz"
-TARBALL_SHA256['aarch64']="cda75346f2c9e09e8a802665745b5a7e2bd6d8584dbf1c86c8c57ef54c4e2d3c"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-arm-pd-v4.26.0.tar.xz"
-TARBALL_SHA256['arm']="868ad59b44098d7175819bdabda0dcd98ddc67af15c153cc9e22797ac77f9dd3"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-i686-pd-v4.26.0.tar.xz"
-TARBALL_SHA256['i686']="8579087c23d759f3ded2c88d8eb707fe9efe524330c8894828f3246cdcc36117"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.26.0/debian-trixie-x86_64-pd-v4.26.0.tar.xz"
-TARBALL_SHA256['x86_64']="e2edc15363395936cf0cba8c440a108458dba58fb496d3d962909d7a8d9777ae"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/debian-trixie-aarch64-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['aarch64']="3834a11cbc6496935760bdc20cca7e2c25724d0cd8f5e4926da8fd5ca1857918"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.29.0/debian-trixie-arm-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['arm']="99bcba87d8d1c66c0de06259ac0a270eb0a20f8b4af39beb0705d28846d78b90"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.29.0/debian-trixie-i686-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['i686']="a388a0531301b033ef5509ab6a50cc886f7f90e7ec9cac02569b45af1900229a"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/debian-trixie-x86_64-pd-v4.29.0.tar.xz"
+TARBALL_SHA256['x86_64']="4b8f33b80a10d734ff935e5934588572f860c0c38a68bf91db59af0580370716"
 
 distro_setup() {
 	# Configure en_US.UTF-8 locale.
