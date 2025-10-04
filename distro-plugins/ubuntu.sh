176697a652a21b872a05138bb01ba5fa4b190ef1
Alif <arfshl@protonmail.com>
2025-10-04 19:32:48 +0700

    Update ubuntu.sh

--- distro-plugins/ubuntu.sh
+++ distro-plugins/ubuntu.sh
@@ -16,8 +16,8 @@ distro_setup() {
 	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 
-	# Configure Firefox PPA.
-	echo "Configuring PPA repository for Firefox..."
+	# Configure Mozilla PPA.
+	echo "Configuring PPA repository for Firefox and Thunderbird..."
 	run_proot_cmd add-apt-repository --yes --no-update ppa:mozillateam/ppa || true
 	cat <<- CONFIG_EOF > ./etc/apt/preferences.d/pin-mozilla-ppa
 	Package: *
