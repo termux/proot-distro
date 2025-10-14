b44a041927a9d2a5eaa688c2c7e4eff71f3cae26
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-10-14 22:25:59 +0300

    ubuntu: fix permission denial during build

--- distro-build/ubuntu.sh
+++ distro-build/ubuntu.sh
@@ -25,9 +25,8 @@ bootstrap_distribution() {
 		chroot "${WORKDIR}/ubuntu-${dist_version}-$(translate_arch "$arch")" apt purge -yq --allow-remove-essential coreutils-from-uutils
 		chroot "${WORKDIR}/ubuntu-${dist_version}-$(translate_arch "$arch")" apt purge -yq --allow-remove-essential rust-coreutils
 		chroot "${WORKDIR}/ubuntu-${dist_version}-$(translate_arch "$arch")" apt clean
-		EOF
-
 		rm -rf "${WORKDIR}/ubuntu-${dist_version}-$(translate_arch "$arch")"/var/lib/apt/lists/*
+		EOF
 
 		archive_rootfs "${ROOTFS_DIR}/ubuntu-${dist_version}-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar.xz" \
 			"ubuntu-${dist_version}-$(translate_arch "$arch")"
