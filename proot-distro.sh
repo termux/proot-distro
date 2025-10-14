085e8e23377dc2006843680ec9090fc14652efb0
Sylirre <107305601+sylirre@users.noreply.github.com>
2025-10-14 22:56:31 +0300

    version 4.31.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -21,7 +21,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="4.30.1"
+PROGRAM_VERSION="4.31.0"
 
 #############################################################################
 #
