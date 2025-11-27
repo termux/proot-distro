33d8ebe780ee81280ca6ecb5ddbb7129ec51456d
Sylirre <sylirre@termux.dev>
2025-11-27 10:14:53 +0000

    pardus: update plugin script

--- distro-plugins/pardus.sh
+++ distro-plugins/pardus.sh
@@ -2,11 +2,11 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 DISTRO_NAME="Pardus"
-DISTRO_COMMENT="Version 'yirmiuc'."
+DISTRO_COMMENT="Version 'yirmibes'."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/pardus-aarch64-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['aarch64']="623794c3831f7bdd1e6cb644e0ab15ae249c2122cc491b8674e63265fc71bcd8"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.29.0/pardus-i686-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['i686']="455783735608c4bbb8d760820afc157d7fd2ecef768e4f111695e2a72736ff38"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/pardus-x86_64-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['x86_64']="90b56d30752f95492d86ceaa99fd30710c4ae9afc4d228ebf8e49992d57bbc1c"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.33.2/pardus-aarch64-pd-v4.33.2.tar.xz"
+TARBALL_SHA256['aarch64']="77969ecd9c08c43e98cb09b2661b04042bd83155966698928d0bc423fc0fd118"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.33.2/pardus-i686-pd-v4.33.2.tar.xz"
+TARBALL_SHA256['i686']="4acb4163121ece6534bc04d4e4fdcae299064caea6e0fd7ade1f6d47f880b9a6"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.33.2/pardus-x86_64-pd-v4.33.2.tar.xz"
+TARBALL_SHA256['x86_64']="d214fa2a640419a3edafc2747bebcc1cd08ab77686fc0ba328f97f46ee9fec24"
