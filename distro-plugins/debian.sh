2dbd7f3c503105c724538d2eea24ec63bd395a54
Alif <arfshl@protonmail.com>
2025-12-18 14:46:16 +0700

    Debian: Update package database after install

--- distro-plugins/debian.sh
+++ distro-plugins/debian.sh
@@ -17,4 +17,6 @@ distro_setup() {
 	# Configure en_US.UTF-8 locale.
 	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+	# Update package database
+	apt update
 }
