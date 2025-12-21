a07b982f4fe901666da4e12532cf9bf10b0aa559
Sylirre <sylirre@termux.dev>
2025-12-21 23:45:50 +0000

    archlinux: update plugin script

--- distro-plugins/archlinux.sh
+++ distro-plugins/archlinux.sh
@@ -4,14 +4,14 @@
 DISTRO_NAME="Arch Linux"
 DISTRO_COMMENT="ARM(64) devices use Arch Linux ARM, i686 uses Arch Linux 32. Both are independent projects. The original Arch usable only by x86_64 devices."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/archlinux-aarch64-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['aarch64']="08d74365213e647c558e561b0a2a7afb6fa3dfe345a1994c62ccac5af1a1cdc6"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.29.0/archlinux-arm-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['arm']="df17fd1058a103ed64811900498c9432abd303eee3eb27cbacab041a14011fba"
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.29.0/archlinux-i686-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['i686']="5fc6240f81c88bb69391c189dd2d0f4f5d9dc9503b400baee3bef060a49ee37c"
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.29.0/archlinux-x86_64-pd-v4.29.0.tar.xz"
-TARBALL_SHA256['x86_64']="8249202836643a4a4f922004c34faa2c3f7d9fec0464ee23b087ad325f1610d9"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.34.2/archlinux-aarch64-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['aarch64']="dabc2382ddcb725969cf7b9e2f3b102ec862ea6e0294198a30c71e9a4b837f81"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.34.2/archlinux-arm-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['arm']="811bc341419c08b68f3c9ee68af546f8195f82660d9a11c54d916c1c353b8d90"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.34.2/archlinux-i686-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['i686']="52d9167658510c504481de63594b55daf9312c169e4134eb2e65e9ce813a85e9"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.34.2/archlinux-x86_64-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['x86_64']="5829c102ff1789d0e026ede65685221433e0b5c18002e70471a52b752c761be2"
 
 distro_setup() {
 	# Fix environment variables on login or su.
