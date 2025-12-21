afe17e3b984126c0fdd2f3d2f312133a012c340a
Sylirre <sylirre@termux.dev>
2025-12-21 23:17:46 +0000

    opensuse: update plugin script

--- distro-plugins/opensuse.sh
+++ distro-plugins/opensuse.sh
@@ -4,14 +4,14 @@
 DISTRO_NAME="OpenSUSE"
 DISTRO_COMMENT="Leap release (16.0). No support for ARM and x86 32bit."
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.31.0/opensuse-aarch64-pd-v4.31.0.tar.xz"
-TARBALL_SHA256['aarch64']="8f5accdfb6404dc3caa0487505366a19c2a0cb742839353f862b583c8033128a"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.31.0/opensuse-arm-pd-v4.31.0.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.34.2/opensuse-aarch64-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['aarch64']="ac161ca81deb7a0418f382d66c2110802b90481cfe0f925b02b69a5642674379"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.34.2/opensuse-arm-pd-v4.34.2.tar.xz"
 TARBALL_SHA256['arm']=""
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.31.0/opensuse-i686-pd-v4.31.0.tar.xz"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v4.34.2/opensuse-i686-pd-v4.34.2.tar.xz"
 TARBALL_SHA256['i686']=""
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.31.0/opensuse-x86_64-pd-v4.31.0.tar.xz"
-TARBALL_SHA256['x86_64']="cc7008ce71993f4f5505a204769c7e6aca19f60da945c762b0639b7c9a00cdd5"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.34.2/opensuse-x86_64-pd-v4.34.2.tar.xz"
+TARBALL_SHA256['x86_64']="6e817d9f188f4a3a173b7a928b45c0b250189ba87c2eb74a953235c8c14a7f58"
 
 distro_setup() {
 	# Lock package filesystem to remove issues regarding zypper dup
