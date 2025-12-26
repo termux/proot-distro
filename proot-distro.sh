eb45040a5c751ca94058bec2f0aef6673707c2cb
Sylirre <sylirre@termux.dev>
2025-12-26 21:25:25 +0000

    version 4.36.0

--- proot-distro.sh
+++ proot-distro.sh
@@ -21,7 +21,7 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
-PROGRAM_VERSION="4.35.0"
+PROGRAM_VERSION="4.36.0"
 
 #############################################################################
 #
