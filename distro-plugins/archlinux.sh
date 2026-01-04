30d241d7a56ce13741bdcd0c7683e0718c252637
Sylirre <sylirre@termux.dev>
2026-01-04 02:11:25 +0000

    archlinux: update plugin script

--- distro-plugins/archlinux.sh
+++ distro-plugins/archlinux.sh
@@ -4,14 +4,14 @@
 DISTRO_NAME="Arch Linux"
 DISTRO_COMMENT="ARM(64) devices use Arch Linux ARM, i686 uses Arch Linux 32. Both are independent projects. The original Arch usable only by x86_64 devices."
 
-TARBALL_URL['aarch64']="https://easycli.sh/proot-distro/archlinux-aarch64-pd-v4.34.2.tar.xz"
-TARBALL_SHA256['aarch64']="dabc2382ddcb725969cf7b9e2f3b102ec862ea6e0294198a30c71e9a4b837f81"
-TARBALL_URL['arm']="https://easycli.sh/proot-distro/archlinux-arm-pd-v4.34.2.tar.xz"
-TARBALL_SHA256['arm']="811bc341419c08b68f3c9ee68af546f8195f82660d9a11c54d916c1c353b8d90"
-TARBALL_URL['i686']="https://easycli.sh/proot-distro/archlinux-i686-pd-v4.34.2.tar.xz"
-TARBALL_SHA256['i686']="52d9167658510c504481de63594b55daf9312c169e4134eb2e65e9ce813a85e9"
-TARBALL_URL['x86_64']="https://easycli.sh/proot-distro/archlinux-x86_64-pd-v4.34.2.tar.xz"
-TARBALL_SHA256['x86_64']="5829c102ff1789d0e026ede65685221433e0b5c18002e70471a52b752c761be2"
+TARBALL_URL['aarch64']="https://easycli.sh/proot-distro/archlinux-aarch64-pd-v4.36.0.tar.xz"
+TARBALL_SHA256['aarch64']="f4318f971e62a5e8407191728b3b92414bcb0f82267774d09d8e25de68acb650"
+TARBALL_URL['arm']="https://easycli.sh/proot-distro/archlinux-arm-pd-v4.36.0.tar.xz"
+TARBALL_SHA256['arm']="8390cf5ecc206556f57f2534a0a00c6e0e5c4ee34a77bf4cc6c981ef120556db"
+TARBALL_URL['i686']="https://easycli.sh/proot-distro/archlinux-i686-pd-v4.36.0.tar.xz"
+TARBALL_SHA256['i686']="589ded6f640e7900534ac25aaa0f7ea7e9a4bae9d98b27c5c34ae30b78e5d6e4"
+TARBALL_URL['x86_64']="https://easycli.sh/proot-distro/archlinux-x86_64-pd-v4.36.0.tar.xz"
+TARBALL_SHA256['x86_64']="7881d975bf07e1803721c780275eaa5ebd8a06da0dd6cbfca49ee625b94a39dc"
 
 distro_setup() {
 	# Fix environment variables on login or su.
